232896d12e2332188568f2fe6d7772f77741c21e
Ed Tewiah <etewiah@hotmail.cim>
2016-11-09 12:05:24 +0100

    Render jumbotron on index page

--- app/controllers/pwb/welcome_controller.rb
+++ app/controllers/pwb/welcome_controller.rb
@@ -2,10 +2,10 @@ require_dependency "pwb/application_controller"
 
 module Pwb
   class WelcomeController < ApplicationController
-    
+
     # GET /welcomes
     def index
-      # @welcomes = Welcome.all
+      @carousel_items = Pwb::Content.where(tag: "landing-carousel").includes(:content_photos, :translations)
     end
 
   end
