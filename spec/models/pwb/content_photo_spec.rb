463d509c6a6b469f690dac2a49adc5fedad8178e
Ed Tewiah <etewiah@hotmail.cim>
2016-11-18 13:32:38 +0100

    Add most basic model spec

--- spec/models/pwb/content_photo_spec.rb
+++ spec/models/pwb/content_photo_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 module Pwb
   RSpec.describe ContentPhoto, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    let(:content_photo) { FactoryGirl.create(:pwb_content_photo) }
+
+    it "has a valid factory" do
+      expect(content_photo).to be_valid
+    end
   end
 end
--- spec/models/pwb/content_spec.rb
+++ spec/models/pwb/content_spec.rb
@@ -1,7 +1,20 @@
-require 'rails_helper'
+# require 'rails_helper'
+require 'spec_helper'
+
 
 module Pwb
   RSpec.describe Content, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    # pending "add some examples to (or delete) #{__FILE__}"
+    let(:content) { FactoryGirl.create(:pwb_content) }
+      # create(:alchemy_element, name: 'headline', create_contents_after_create: true) }
+
+    it "has a valid factory" do
+      expect(content).to be_valid
+    end
+
+    # it "is invalid without a key" do
+    #   ::FactoryGirl.build(:pwb_content, key: nil).should_not be_valid
+    # end
+
   end
 end
