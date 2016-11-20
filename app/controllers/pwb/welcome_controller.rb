167c9a2729d6cb03d879a4f14fa1ca28e6a32032
Ed Tewiah <etewiah@hotmail.cim>
2016-11-20 19:11:01 +0100

    Retrieve and display prop summaries on landing page

--- app/controllers/pwb/welcome_controller.rb
+++ app/controllers/pwb/welcome_controller.rb
@@ -9,6 +9,7 @@ module Pwb
       @content_area_cols = Content.where(tag: 'content-area-cols').order('sort_order')
       @about_us = Content.find_by_key('aboutUs')
       @properties_for_sale = Prop.for_sale.visible.order('highlighted DESC').limit 9
+      @properties_for_rent = Prop.for_rent.visible.order('highlighted DESC').limit 9
     end
   end
 end
--- app/models/pwb/prop.rb
+++ app/models/pwb/prop.rb
@@ -8,5 +8,21 @@ module Pwb
     scope :for_sale, -> () { where for_sale: true }
     scope :visible, -> () { where visible: true }
 
+    def has_garage
+      self.count_garages && (self.count_garages > 0)
+    end
+
+
+    # def contextual_show_path rent_or_sale
+    #   unless rent_or_sale
+    #     # where I am displaying items searched by ref number, won't know if its for sale or rent beforehand
+    #     rent_or_sale = self.for_rent ? "for_rent" : "for_sale"
+    #   end
+    #   if rent_or_sale == "for_rent"
+    #     return Rails.application.routes.url_helpers.property_show_for_rent_path(locale: I18n.locale, id: self.id, url_friendly_title: self.url_friendly_title)
+    #   else
+    #     return Rails.application.routes.url_helpers.property_show_for_sale_path(locale: I18n.locale, id: self.id, url_friendly_title: self.url_friendly_title)
+    #   end
+    # end
   end
 end
