a81db45107566f88213285a8c444bc1fb6dcb971
Ed Tewiah <etewiah@hotmail.cim>
2016-11-20 18:20:35 +0100

    Retrieve props by scope

--- app/controllers/pwb/welcome_controller.rb
+++ app/controllers/pwb/welcome_controller.rb
@@ -4,11 +4,11 @@ module Pwb
   class WelcomeController < ApplicationController
     # GET /welcomes
     def index
-      @carousel_items = Pwb::Content.where(tag: 'landing-carousel')
+      @carousel_items = Content.where(tag: 'landing-carousel')
       # .includes(:content_photos, :translations)
       @content_area_cols = Content.where(tag: 'content-area-cols').order('sort_order')
       @about_us = Content.find_by_key('aboutUs')
-      # @about_us = Content.get_raw_by_key("aboutUs")
+      @properties_for_sale = Prop.for_sale.visible.order('highlighted DESC').limit 9
     end
   end
 end
--- app/models/pwb/prop.rb
+++ app/models/pwb/prop.rb
@@ -2,5 +2,11 @@ module Pwb
   class Prop < ApplicationRecord
     translates :title, :description
     globalize_accessors locales: [:en, :ca, :es, :fr, :ar]
+
+    scope :for_rent, -> () { where('for_rent_short_term OR for_rent_long_term') }
+    # couldn't do above if for_rent_short_term was a flatshihtzu boolean
+    scope :for_sale, -> () { where for_sale: true }
+    scope :visible, -> () { where visible: true }
+
   end
 end
