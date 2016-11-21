c2e9dfbaf8c1c6350eb5f95cb9d0a08bfe6e7792
Ed Tewiah <etewiah@hotmail.cim>
2016-11-21 14:08:55 +0100

    Add basic header and footer

--- app/controllers/pwb/application_controller.rb
+++ app/controllers/pwb/application_controller.rb
@@ -1,5 +1,18 @@
 module Pwb
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    before_filter :current_agency, :sections
+
+    private
+
+    def current_agency
+      @current_agency ||= (Agency.last || Agency.create)
+    end
+
+    def sections
+      @sections ||= []
+    end
+
   end
 end
