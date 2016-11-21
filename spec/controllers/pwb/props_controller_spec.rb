d945d8111084249db919741d52b94a3addc99cbb
Ed Tewiah <etewiah@hotmail.cim>
2016-11-21 09:44:07 +0100

    Failed attempt to test props controller

--- spec/controllers/pwb/props_controller_spec.rb
+++ spec/controllers/pwb/props_controller_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 module Pwb
   RSpec.describe PropsController, type: :controller do
+    routes { Pwb::Engine.routes }
 
+    # describe 'GET #show_for_rent' do
+
+    #   it 'renders correct template' do
+    #     # byebug
+    #     expect(get(:show_for_rent)).to render_template('pwb/welcome/show_for_rent')
+    #     # above results in error:  
+    #     # ActionController::UrlGenerationError Exception: No route matches {:action=>"prop_show_for_rent", :controller=>"pwb/props"}
+    #   end
+    # end
   end
 end
