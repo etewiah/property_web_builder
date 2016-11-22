c34c1a433d22e81cd4bf1346fed2b991a54fbfb7
Ed Tewiah <etewiah@hotmail.cim>
2016-11-22 18:18:14 +0100

    Add tests for props controller

--- spec/controllers/pwb/props_controller_spec.rb
+++ spec/controllers/pwb/props_controller_spec.rb
@@ -3,15 +3,38 @@ require 'rails_helper'
 module Pwb
   RSpec.describe PropsController, type: :controller do
     routes { Pwb::Engine.routes }
+    let(:prop_for_long_term_rent) { FactoryGirl.create(:pwb_prop, :available_for_long_term_rent,
+                                                       price_rental_monthly_current_cents: 100000) }
+    let(:prop_for_sale) { FactoryGirl.create(:pwb_prop, :available_for_sale,
+                                             price_sale_current_cents: 10000000) }
 
-    # describe 'GET #show_for_rent' do
+    describe 'GET #show_for_rent' do
+      it 'renders correct template' do
+        expect(get(:show_for_rent, params: {
+                     id: prop_for_long_term_rent.id,
+                     url_friendly_title: "tt"
+        })).to render_template('pwb/props/show')
+      end
+    end
 
-    #   it 'renders correct template' do
-    #     # byebug
-    #     expect(get(:show_for_rent)).to render_template('pwb/welcome/show_for_rent')
-    #     # above results in error:  
-    #     # ActionController::UrlGenerationError Exception: No route matches {:action=>"prop_show_for_rent", :controller=>"pwb/props"}
-    #   end
-    # end
+    describe 'GET #show_for_sale' do
+      context 'with id of for sale prop' do
+        it 'renders correct template' do
+          expect(get(:show_for_sale, params: {
+                       id: prop_for_sale.id,
+                       url_friendly_title: "tt"
+          })).to render_template('pwb/props/show')
+        end
+      end
+      context 'with id of for rent prop' do
+        it 'renders correct template' do
+          expect(get(:show_for_sale, params: {
+                       id: prop_for_long_term_rent.id,
+                       url_friendly_title: "tt"
+          })).to render_template('pwb/props/not_found')
+        end
+      end
+    end
+
   end
 end
--- spec/factories/pwb_props.rb
+++ spec/factories/pwb_props.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :pwb_prop, class: 'Pwb::Prop' do
-    
+    title_en 'A property for '
+    trait :available_for_sale do
+      for_sale true
+      visible true
+    end    
+    trait :available_for_long_term_rent do
+      for_rent_long_term true
+      visible true
+    end    
   end
 end
