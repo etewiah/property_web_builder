bbc71de9a04ec9a2da588b969404200a9c11ddee
Ed Tewiah <etewiah@hotmail.cim>
2016-11-22 14:10:22 +0100

    Use contextual_show_path for links to props

--- app/models/pwb/prop.rb
+++ app/models/pwb/prop.rb
@@ -24,16 +24,33 @@ module Pwb
       end
     end
 
-    # def contextual_show_path rent_or_sale
-    #   unless rent_or_sale
-    #     # where I am displaying items searched by ref number, won't know if its for sale or rent beforehand
-    #     rent_or_sale = self.for_rent ? "for_rent" : "for_sale"
-    #   end
-    #   if rent_or_sale == "for_rent"
-    #     return Rails.application.routes.url_helpers.property_show_for_rent_path(locale: I18n.locale, id: self.id, url_friendly_title: self.url_friendly_title)
-    #   else
-    #     return Rails.application.routes.url_helpers.property_show_for_sale_path(locale: I18n.locale, id: self.id, url_friendly_title: self.url_friendly_title)
-    #   end
-    # end
+    # 
+    def ordered_photo_url number
+      unless self.property_photos.length >= number
+        return "https://placeholdit.imgix.net/~text?txtsize=38&txt=&w=550&h=400&txttrack=0"
+      end
+      return self.property_photos[number - 1].image.url
+    end
+
+    def url_friendly_title
+      # used in constructing seo friendly url
+      if self.title && self.title.length > 2
+        return self.title.parameterize
+      else
+        return "show"
+      end
+    end
+
+    def contextual_show_path rent_or_sale
+      unless rent_or_sale
+        # where I am displaying items searched by ref number, won't know if its for sale or rent beforehand
+        rent_or_sale = self.for_rent ? "for_rent" : "for_sale"
+      end
+      if rent_or_sale == "for_rent"
+        return Pwb::Engine.routes.url_helpers.prop_show_for_rent_path(locale: I18n.locale, id: self.id, url_friendly_title: self.url_friendly_title)
+      else
+        return Pwb::Engine.routes.url_helpers.prop_show_for_sale_path(locale: I18n.locale, id: self.id, url_friendly_title: self.url_friendly_title)
+      end
+    end
   end
 end
