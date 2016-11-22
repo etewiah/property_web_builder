3e9698c174af9790e22430705744ea4addb54c54
Ed Tewiah <etewiah@hotmail.cim>
2016-11-22 19:42:41 +0100

    Add section_tab helper

--- app/helpers/pwb/application_helper.rb
+++ app/helpers/pwb/application_helper.rb
@@ -1,4 +1,23 @@
 module Pwb
   module ApplicationHelper
+    def section_tab(section_info)
+      begin
+        target_path = send(section_info[:link_path], {locale: locale})
+
+      rescue NoMethodError
+        target_path = '/'
+      # rescue Exception => e
+      end
+      # binding.pry
+      style_class = 'selected' if current_page?( target_path )
+      # section_info['link_path'](locale: locale))
+      html = <<-HTML
+      <li class="#{style_class}">
+      #{link_to I18n.t('navbar.'+section_info[:link_key]), target_path}
+      </li>
+      HTML
+
+      html.html_safe
+    end
   end
 end
---
+++ spec/helpers/pwb/application_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the ApplicationHelper. For example:
+#
+# describe ApplicationHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+module Pwb
+  RSpec.describe ApplicationHelper, type: :helper do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
