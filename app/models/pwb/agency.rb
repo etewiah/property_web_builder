65a37d202ee03aaff78b00993d79175cffb26ea9
Ed Tewiah <etewiah@hotmail.cim>
2016-11-24 22:45:22 +0100

    Ensure only 1 agency can be created

--- app/models/pwb/agency.rb
+++ app/models/pwb/agency.rb
@@ -1,5 +1,6 @@
 module Pwb
   class Agency < ApplicationRecord
+    before_create :confirm_singularity
     # has_many :users
 
     # foreign_key of primary_address_id is col here on agency
@@ -9,5 +10,20 @@ module Pwb
     # def show_contact_map
     #   return self.primary_address.present?
     # end
+
+    def views_folder
+      views_folder = "/pwb/themes/standard"
+      # if self.site_template.present? && self.site_template.views_folder
+      #   views_folder = self.site_template.views_folder
+      # end
+      return views_folder
+    end
+
+
+    private
+
+    def confirm_singularity
+      raise Exception.new("There can be only one.") if Agency.count > 0
+    end
   end
 end
