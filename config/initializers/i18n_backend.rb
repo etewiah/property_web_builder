f9b649faec11cf0ca6d2c2e92709d373b8a523a8
Ed Tewiah <etewiah@hotmail.cim>
2016-11-30 23:25:58 +0100

    Fix error in initializing i18n

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -2,7 +2,10 @@ require 'i18n/backend/active_record'
 # I18n.backend = I18n::Backend::ActiveRecord.new
 Translation  = I18n::Backend::ActiveRecord::Translation
 
-if Translation.table_exists?
+# if Translation.table_exists?
+# in the context of an engine, above returns false 
+# even when the table exists
+if ActiveRecord::Base.connection.data_source_exists? 'translations'
   I18n.backend = I18n::Backend::ActiveRecord.new
 
   I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
