610a2d039bb217a92f209cb6245284f0ee2845ef
Ed Tewiah <etewiah@hotmail.cim>
2016-12-04 15:00:23 +0100

    Add blank admin view

--- spec/controllers/pwb/admin_panel_controller_spec.rb
+++ spec/controllers/pwb/admin_panel_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 module Pwb
   RSpec.describe AdminPanelController, type: :controller do
+    routes { Pwb::Engine.routes }
+
+    describe 'GET #show' do
+      it 'renders correct template' do
+        expect(get(:show)).to render_template('pwb/admin_panel/show')
+      end
+    end
 
   end
 end
