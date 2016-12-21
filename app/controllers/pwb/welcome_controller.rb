41d533525b4802201cd2c6bd63dd69b6d68d18e7
Ed Tewiah <etewiah@hotmail.cim>
2016-12-21 11:36:18 +0100

    Add themes folder and move welcome/index erb to it

--- app/controllers/pwb/welcome_controller.rb
+++ app/controllers/pwb/welcome_controller.rb
@@ -2,7 +2,15 @@ require_dependency 'pwb/application_controller'
 
 module Pwb
   class WelcomeController < ApplicationController
-    # GET /welcomes
+
+    before_filter :set_theme_path
+
+    def set_theme_path
+      prepend_view_path "#{Pwb::Engine.root}/app/themes/chic/views/"
+      # below allows themes installed in Rails app consuming Pwb to work
+      prepend_view_path "#{Rails.root}/app/themes/default/views/"
+    end
+
     def index
       @carousel_items = Content.where(tag: 'landing-carousel')
       @carousel_speed = 3000
@@ -11,6 +19,9 @@ module Pwb
       @about_us = Content.find_by_key('aboutUs')
       @properties_for_sale = Prop.for_sale.visible.order('highlighted DESC').limit 9
       @properties_for_rent = Prop.for_rent.visible.order('highlighted DESC').limit 9
+
+      return render "welcome/index"
+
     end
   end
 end
