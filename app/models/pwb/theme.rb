718fbf417afc5ef97fdbf5e397def6bd3782c470
Ed Tewiah <etewiah@hotmail.cim>
2016-12-22 09:35:50 +0100

    Read theme data from config.json

--- app/models/pwb/theme.rb
+++ app/models/pwb/theme.rb
@@ -1,11 +1,12 @@
 module Pwb
-  class Theme < ActiveHash::Base
+  class Theme <  ActiveJSON::Base
+    set_root_path "#{Pwb::Engine.root}/app/themes"
+    set_filename "config"
+    # use_multiple_files
+    # set_filenames "default"
+
     include ActiveHash::Associations
     has_one :agency, :foreign_key => "site_template_id", :class_name => "Pwb::Agency"
 
-    self.data = [
-      {:id => 1, :name => "default"},
-      {:id => 2, :name => "chic"}
-    ]
   end
 end
