9ef8466e8cd0b8b606cc8202be34c702e1691373
Ed Tewiah <etewiah@hotmail.cim>
2016-12-31 20:27:32 +0100

    Fix default redirect paths for devise

--- app/controllers/pwb/devise_controller.rb
+++ app/controllers/pwb/devise_controller.rb
@@ -53,5 +53,27 @@ module Pwb
     #   end
     # end
 
+    # https://github.com/plataformatec/devise/blob/master/lib/devise/controllers/helpers.rb
+
+    # Method used by sessions controller to sign out a user. You can overwrite
+    # it in your ApplicationController to provide a custom hook for a custom
+    # scope. Notice that differently from +after_sign_in_path_for+ this method
+    # receives a symbol with the scope, and not the resource.
+    #
+    # Overwriting the sign_out redirect path method
+    # By default it is the root_path.
+    def after_sign_out_path_for(resource_or_scope)
+      # scope = Devise::Mapping.find_scope!(resource_or_scope)
+      # router_name = Devise.mappings[scope].router_name
+      # context = router_name ? send(router_name) : self
+      # context.respond_to?(:root_path) ? context.root_path : "/"
+      pwb.home_path
+    end
+
+
+    def after_sign_in_path_for(resource_or_scope)
+      pwb.home_path
+      # stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
+    end
   end
 end
---
+++ spec/features/pwb/sessions_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+module Pwb
+  RSpec.describe "Sessions", type: :feature do
+    before(:all) do
+      @agency = FactoryGirl.create(:pwb_agency, company_name: 'my re')
+      @admin_user = User.create!(email: "user@example.org", password: "very-secret", admin:true)
+    end
+
+
+    scenario 'with valid credentials' do
+      visit('/users/sign_in')
+      fill_in('Email', :with => @admin_user.email)
+      fill_in('Password', :with => @admin_user.password)
+      click_button('Sign in')
+      expect(current_path).to eq("/en")
+    end
+
+    scenario 'with invalid password' do
+      visit('/users/sign_in')
+      fill_in('Email', :with => @admin_user.email)
+      fill_in('Password', :with => 'bananas')
+      click_button('Sign in')
+      expect(page).to have_content 'Invalid email or password'
+    end
+
+
+    after(:all) do
+      @agency.destroy
+      @admin_user.destroy
+    end
+  end
+end
