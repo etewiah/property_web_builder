6cb1a3495a60726e09e5cdfa09d056deade130d8
Ed Tewiah <etewiah@hotmail.cim>
2017-01-31 17:39:41 +0100

    Fix sticky footer

--- app/controllers/pwb/welcome_controller.rb
+++ app/controllers/pwb/welcome_controller.rb
@@ -7,7 +7,7 @@ module Pwb
       @carousel_speed = 3000
       # .includes(:content_photos, :translations)
       @content_area_cols = Content.where(tag: 'content-area-cols').order('sort_order')
-      @about_us = Content.find_by_key('aboutUs')
+      # @about_us = Content.find_by_key('aboutUs')
       @properties_for_sale = Prop.for_sale.visible.order('highlighted DESC').limit 9
       @properties_for_rent = Prop.for_rent.visible.order('highlighted DESC').limit 9
 
