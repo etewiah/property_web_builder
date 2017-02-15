c1b67dce91d2a6665c73b2b8ab04f1e4a9d600f0
Ed Tewiah <etewiah@hotmail.cim>
2017-02-15 18:02:27 +0100

    Disable CORS for api_public controller

--- app/controllers/pwb/api_public/v1/props_controller.rb
+++ app/controllers/pwb/api_public/v1/props_controller.rb
@@ -5,5 +5,32 @@ module Pwb
     # Skipping action below allows me to browse to endpoint
     # without having set mime type
     skip_before_action :ensure_valid_accept_media_type
+
+
+    before_filter :cors_preflight_check
+    after_filter :cors_set_access_control_headers
+
+    # For all responses in this controller, return the CORS access control headers.
+
+    def cors_set_access_control_headers
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Max-Age'] = "1728000"
+    end
+
+    # If this is a preflight OPTIONS request, then short-circuit the
+    # request, return only the necessary headers and return an empty
+    # text/plain.
+
+    def cors_preflight_check
+      if request.method == :options
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+        headers['Access-Control-Max-Age'] = '1728000'
+        render :text => '', :content_type => 'text/plain'
+      end
+    end
+
   end
 end
