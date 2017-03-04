2bada4a6bf58cf0621094d4f3c1af9fc2f2482de
Ed Tewiah <etewiah@hotmail.cim>
2017-03-04 22:58:07 +0100

    Add content_photo_uploader spec

--- spec/factories/pwb_content_photos.rb
+++ spec/factories/pwb_content_photos.rb
@@ -1,4 +1,7 @@
+# https://til.codes/testing-carrierwave-file-uploads-with-rspec-and-factorygirl/
 FactoryGirl.define do
   factory :pwb_content_photo, class: 'Pwb::ContentPhoto' do
+    path_to_file = Pwb::Engine.root.join("db/example_images/flat_balcony.jpg")
+    # photo Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/myfiles/myfile.jpg')))
   end
 end
