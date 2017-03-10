50b8cce4fc299bdee043526e823c842b5f8d9924
Ed Tewiah <etewiah@hotmail.cim>
2017-03-10 16:21:59 +0100

    Make property search spec theme specific

--- spec/factories/pwb_agencies.rb
+++ spec/factories/pwb_agencies.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
   factory :pwb_agency, class: 'Pwb::Agency' do
     initialize_with { Pwb::Agency.unique_instance() }
+    trait :theme_default do
+      theme_name "default"
+    end
+    trait :theme_berlin do
+      theme_name "berlin"
+      # visible true
+    end
   end
 end
--- spec/features/pwb/theme_default/prop_search_spec.rb
+++ spec/features/pwb/theme_default/prop_search_spec.rb
@@ -1,43 +1,39 @@
 require 'rails_helper'
 
 module Pwb
-  RSpec.describe "Prop Search", type: :feature, js: true do
+  RSpec.describe "Default theme property search", type: :feature, js: true do
     before(:all) do
-      # @agency = FactoryGirl.create(:pwb_agency, company_name: 'my re')
-      # @admin_user = User.create!(email: "user@example.org", password: "very-secret", admin:true)
+      @agency = FactoryGirl.create(:pwb_agency, :theme_default, company_name: 'my re')
+      # @agency.theme_name = "berlin"
+      # @agency.save!
+
       @prop_for_long_term_rent = FactoryGirl.create(
         :pwb_prop,
         :long_term_rent,
         price_rental_monthly_current_cents: 100_000,
         reference: "ref_pfltr"
       )
-      # @prop_for_sale =  FactoryGirl.create(
-      #   :pwb_prop,
-      #   :sale,
-      #   price_sale_current_cents: 10000000,
-      #   :reference => "ref_pf"
-      # )
     end
 
     scenario 'property search works' do
+
       visit('/en/rent')
       # puts current_url
       # require 'pry'; binding.pry
       # save_and_open_page
       # save_and_open_screenshot
-
       expect(page).to have_css(".property-item", count: Prop.visible.for_rent.count)
       # Capybara.ignore_hidden_elements = false
       # passing visible: false below would be like setting above
       select('2,500', from: 'search_for_rent_price_from', visible: false)
+
       click_button('Search')
       expect(page).to have_css(".property-item", count:  Prop.visible.for_rent.for_rent_price_from(250000).count)
       # expect(current_path).to eq("/en")
     end
 
     after(:all) do
-      # @agency.destroy
-      # @admin_user.destroy
+      @agency.destroy
       @prop_for_long_term_rent.destroy
       # @prop_for_sale.destroy
     end
