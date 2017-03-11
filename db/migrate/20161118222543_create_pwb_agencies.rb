6baef1a7a3de15ea17fd7a6655134ac523e76143
Carlos Villalba <carlos.villalba.j@gmail.com>
2017-03-11 10:12:27 -0500

    @cvillalba change migration to define json column in a different way

--- db/migrate/20161118222543_create_pwb_agencies.rb
+++ db/migrate/20161118222543_create_pwb_agencies.rb
@@ -25,7 +25,7 @@ class CreatePwbAgencies < ActiveRecord::Migration[5.0]
       t.integer :payment_plan_id
 
       t.integer :site_template_id
-      t.json :site_configuration, default: {}
+      t.columnjson :site_configuration, :json, default: {}
 
       t.text :available_locales, array: true, default: []
       t.text :supported_locales, array: true, default: []
