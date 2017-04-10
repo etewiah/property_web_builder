15504d59bbb63840504aa98e84694a352dd55f56
Ed Tewiah <etewiah@hotmail.cim>
2017-04-10 17:49:14 +0200

    Fix import properties spec

--- spec/services/pwb/import_properties_spec.rb
+++ spec/services/pwb/import_properties_spec.rb
@@ -8,7 +8,7 @@ module Pwb
     #   @file = fixture_file_upload('../to_import/pwb-properties.csv', 'text/csv')
     # end
 
-    let(:file) do
+    let(:csv_file) do
       fixture_file_upload(
         "/to_import/pwb-properties.csv",
         'text/csv'
@@ -34,7 +34,9 @@ module Pwb
     it "imports a valid csv file" do
       # allow(ImportProperties).to_receive(:add_meal_to_order)
       # expect(prop).to receive(:update_attributes).with(status: "served", customer_id: 123)
-      expect { ImportProperties.new(file).import_csv }.to change { Prop.count }.by 4
+      # expect { ImportProperties.new(csv_file).import_csv }.to change { Prop.count }.by 4
+      parsed_properties = ImportProperties.new(csv_file).import_csv
+      expect(parsed_properties.count).to eq(4)
     end
 
   end
