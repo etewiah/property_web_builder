58bfa8de383180355ee82c20d212483bbe4c8a13
Ed Tewiah <etewiah@hotmail.cim>
2017-04-20 17:09:56 +0200

    Allow setting of defaults when mapping imports

--- app/services/pwb/import_mapper.rb
+++ app/services/pwb/import_mapper.rb
@@ -9,10 +9,20 @@ module Pwb
     end
 
     def map_property mls_property
+      # mappings is a hash of MLS fieldnames and the equivalent fieldname in pwb
       mappings = mls_mapping.mappings
-      mapped_property = mls_property.to_hash.map {|k, v| [mappings[k], v] }.to_h
 
-      return mapped_property.except(nil)
+      # mapped_property_old = mls_property.to_hash.map {|k, v| [mappings[k], v] }.to_h
+      # return mapped_property_old.except(nil)
+
+      mapped_property = {}
+      mappings.each do |mapping|
+        mapped_property[mapping[1]["fieldName"]] = mls_property[mapping[0]].blank? ? mapping[1]["default"] : mls_property[mapping[0]]
+      end
+
+      # TODO - figure out way of importing extras
+
+      return mapped_property
     end
 
 
