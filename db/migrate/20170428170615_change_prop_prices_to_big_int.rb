086c23998376867d6f6339baf07d7f2f27fe5b37
Ed Tewiah <etewiah@hotmail.cim>
2017-04-28 19:12:45 +0200

    Change prop prices to bigint

---
+++ db/migrate/20170428170615_change_prop_prices_to_big_int.rb
@@ -0,0 +1,6 @@
+class ChangePropPricesToBigInt < ActiveRecord::Migration[5.0]
+  def change
+    change_column :pwb_props, :price_sale_current_cents, :integer, limit: 8
+    change_column :pwb_props, :price_sale_original_cents, :integer, limit: 8
+  end  
+end
