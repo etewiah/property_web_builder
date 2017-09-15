1192fac4c8f2e613e3d1df4a400ece60b860c092
Ed Tewiah <etewiah@hotmail.cim>
2017-09-15 11:35:39 +0200

    Support images in content_translations_seeder

--- lib/pwb/content_translations_seeder.rb
+++ lib/pwb/content_translations_seeder.rb
@@ -25,8 +25,11 @@ module Pwb
           page_setup.pages.each do |page|
             page_setup.fragment_configs.each do |fragment_config|
               begin
-                if yml[locale][page.slug][fragment_config["label"]]
-                  set_page_block_content locale, page.slug, fragment_config, yml[locale][page.slug][fragment_config["label"]]
+                fragment_label = fragment_config["label"]
+                # Items in each locale seed file are nested as
+                # page_slug/fragment_label and then the block labels
+                if yml[locale][page.slug][fragment_label]
+                  set_page_block_content locale, page.slug, fragment_config, yml[locale][page.slug][fragment_label]
                 end
               rescue NoMethodError => e
 
@@ -36,11 +39,14 @@ module Pwb
         end
       end
 
-      def set_page_block_content locale, page_slug, fragment_config, content_for_page_fragment
+      def set_page_block_content locale, page_slug, fragment_config, seed_content
         page = Pwb::Page.find_by_slug page_slug
+
+        # fragment_label uniquely identifies a fragment
+        # and is also the name of the corresponding partial
         fragment_label = fragment_config["label"]
 
-        # ensure path exists in details col
+        # ensure path exists in details col of page
         unless page.details["fragments"].present?
           page.details["fragments"] = {}
         end
@@ -48,14 +54,21 @@ module Pwb
           page.details["fragments"][fragment_label] = {}
         end
 
+        # container for json to be attached to page details
         content_for_pf_locale = {"blocks" => {}}
         # {"blocks"=>{"title_a"=>{"content"=>"about our agency"}, "content_a"=>{"content"=>""}}}
-        fragment_config["editorBlocks"].each do |colBlocks|
-          colBlocks.each do |rowBlocks|
-            row_block_label = rowBlocks["label"]
+        fragment_config["editorBlocks"].each do |configColBlocks|
+          configColBlocks.each do |configRowBlock|
+            row_block_label = configRowBlock["label"]
             row_block_content = ""
-            if content_for_page_fragment[row_block_label]
-              row_block_content = content_for_page_fragment[row_block_label]
+            # find the content for current block from within the seed content
+            if seed_content[row_block_label]
+              if configRowBlock["isImage"]
+                photo = create_fragment_photo seed_content[row_block_label]
+                row_block_content = photo.present? ? photo.image.url : ""
+              else
+                row_block_content = seed_content[row_block_label]
+              end
             end
             content_for_pf_locale["blocks"][row_block_label] = {"content"=>row_block_content}
           end
@@ -72,6 +85,27 @@ module Pwb
         page.save!
 
       end
+
+
+      def create_fragment_photo photo_file
+        if ENV["RAILS_ENV"] == "test"
+          # don't create photos for tests
+          return nil
+        end
+        begin
+          photo = Pwb::ContentPhoto.create
+          photo.image = Pwb::Engine.root.join(photo_file).open
+          photo.save!
+        rescue Exception => e
+          # log exception to console
+          p e
+          if photo
+            photo.destroy!
+          end
+        end
+        return photo
+      end
+
 
     end
   end
---
+++ spec/libraries/pwb/content_translations_seeder_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'pwb/content_translations_seeder'
+
+module Pwb
+  RSpec.describe 'ContentTranslationsSeeder' do
+    before(:all) do
+      # I18n::Backend::ActiveRecord::Translation.destroy_all
+      # Pwb::Seeder.seed!
+      Pwb::ContentTranslationsSeeder.seed_content_translations!
+    end
+
+    it 'creates our_agency content' do
+      about_us_page = Pwb::Page.find_by_slug "about-us"
+      expect(about_us_page.details["fragments"]["our_agency"]["en"]["blocks"].count).to eq(3)
+      expect(about_us_page.details["fragments"]["our_agency"]["es"]["blocks"].count).to eq(3)
+    end
+
+
+ 
+    # it 'creates i18n translations' do
+    #   es_translations = I18n::Backend::ActiveRecord::Translation.where(locale: "es")
+    #   en_translations = I18n::Backend::ActiveRecord::Translation.where(locale: "en")
+    #   expect(es_translations.count).to eq(en_translations.count)
+    #   expect(es_translations.count).to be > 0
+    # end
+
+    # it 'creates a landing page hero entry' do
+    #   expect(Pwb::Content.find_by_key('landingCarousel1')).to be_present
+    # end
+
+    # it 'creates an about_us entry' do
+    #   expect(Pwb::Content.find_by_key('aboutUs')).to be_present
+    # end
+
+    # it 'creates 3 content-area-cols' do
+    #   expect(Pwb::Content.where(tag: 'content-area-cols').count).to eq(3)
+    # end
+
+    # it 'creates 4 prop entries' do
+    #   expect(Pwb::Prop.count).to eq(4)
+    # end
+  end
+end
