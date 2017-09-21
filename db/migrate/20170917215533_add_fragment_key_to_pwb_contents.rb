0259044d0ce73ce454be2ebd958dbd65c06be5cc
Ed Tewiah <etewiah@hotmail.cim>
2017-09-21 08:58:44 +0200

    Rename migration

--- db/migrate/20170917215533_add_fragment_key_to_pwb_contents.rb
+++ db/migrate/20170917215533_add_fragment_key_to_pwb_contents.rb
@@ -1,4 +1,4 @@
-class AddExtraColsToPwbContents < ActiveRecord::Migration[5.1]
+class AddFragmentKeyToPwbContents < ActiveRecord::Migration[5.1]
   def change
     add_column :pwb_contents, :fragment_key, :string, index: true
     # add_column :pwb_contents, :visible_on_page, :boolean, default: true
