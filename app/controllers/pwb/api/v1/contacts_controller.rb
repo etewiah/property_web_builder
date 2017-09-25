a69fdff3be1825bb557a0815e4bdf0643288de42
Ed Tewiah <etewiah@hotmail.cim>
2017-09-25 11:08:11 +0200

    Add contacts_controller methods

--- app/controllers/pwb/api/v1/contacts_controller.rb
+++ app/controllers/pwb/api/v1/contacts_controller.rb
@@ -3,23 +3,39 @@ module Pwb
 
     respond_to :json
 
+    def create
+      Pwb::Contact.create create_contact_params
+    end
+
+    def update
+      contact = Pwb::Contact.find_by_id params[:id]
+      contact.update update_contact_params
+      return render json: contact.as_json
+    end
+    
+    def show
+      contact = Pwb::Contact.find_by_id params[:id]
+      return render json: contact.as_json
+    end
+
     def index
       contacts = Pwb::Contact.all
-      return render json:         contacts.as_json
-      # # Globalize.fallbacks = {:ru => [:en]}
-      # # top_nav_links, footer_links = nil
-      # locale = params[:locale] || :en
-      # I18n.with_locale(locale) do
-      #   top_nav_links = Link.ordered_top_nav
-      #   footer_links = Link.ordered_footer
-      #   render json: {
-      #     footer_links: footer_links.as_json,
-      #     top_nav_links: top_nav_links.as_json
-      #   }
-      # end
+      return render json: contacts.as_json
     end
 
     private
+
+    def create_contact_params
+      params.require(:details).permit(
+        :first_name, :last_name
+      )
+    end
+
+    def update_contact_params
+      params.require(:details).permit(
+        :first_name, :last_name
+      )
+    end
 
   end
 end
