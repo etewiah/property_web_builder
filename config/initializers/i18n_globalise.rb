0937e992e40e4be42c484498c593eb0ec2b7fdb5
Ed Tewiah <etewiah@hotmail.cim>
2017-10-29 21:05:03 +0000

    Add Italian translations

--- config/initializers/i18n_globalise.rb
+++ config/initializers/i18n_globalise.rb
@@ -1,3 +1,4 @@
-I18n.available_locales = [:en, :es, :fr, :de, :ru, :pt]
+I18n.available_locales = [:en, :ca, :es, :fr, :ar, :de, :ru, :pt, :vi, :tr, :it, :nl]
+# I18n.available_locales = [:en, :es, :fr, :de, :ru, :pt]
 
 Globalize.fallbacks = {:de => [:en],:es => [:en], :ru => [:en]}
