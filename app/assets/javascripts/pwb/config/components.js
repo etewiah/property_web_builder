3aa644c0bed502d659deca87c8b7c939d43bc367
Ed Tewiah <etewiah@hotmail.cim>
2017-12-31 20:48:10 +0100

    Add simple list component

--- app/assets/javascripts/pwb/config/components.js
+++ app/assets/javascripts/pwb/config/components.js
@@ -8,11 +8,43 @@
     }
   })
 
+
+
+
   Vue.component('simple-form', {
     template: '#simple-form',
     data: function() {
       return {
+        dialog: false,
+
+      }
+    }
+  })    
+
+  const simpleList = Vue.component('simple-list', {
+    template: '#simple-list',
+  //   data: function() {
+  //     return {
+  //       dialog: false,
+
+  //     }
+  //   }
+  // })
+
+  // Vue.component('simple-form', {
+  //   template: '#simple-form',
+    data: function() {
+      return {
         props: null,
+       listItems: [
+          { header: 'Today' },
+          { avatar: '/static/doc-images/lists/1.jpg', title: 'Brunch this weekend?', subtitle: "<span class='grey--text text--darken-2'>Ali Connors</span> — I'll be in your neighborhood doing errands this weekend. Do you want to hang out?" },
+          { divider: true, inset: true },
+          { avatar: '/static/doc-images/lists/2.jpg', title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>', subtitle: "<span class='grey--text text--darken-2'>to Alex, Scott, Jennifer</span> — Wish I could come, but I'm out of town this weekend." },
+          { divider: true, inset: true },
+          { avatar: '/static/doc-images/lists/3.jpg', title: 'Oui oui', subtitle: "<span class='grey--text text--darken-2'>Sandra Adams</span> — Do you have Paris recommendations? Have you ever been?" }
+        ],
+
         rightDrawer: false,
         right: true,
         search: '',
