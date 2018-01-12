74aceab2ea1c4ebdeb2126b1f1895c3cbe207b0b
Ed Tewiah <etewiah@hotmail.cim>
2018-01-12 18:16:07 +0100

    Refactor page factory for rspec

--- spec/features/pwb/theme_default/page_part_content_spec.rb
+++ spec/features/pwb/theme_default/page_part_content_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
-# TODO: - use page object here too
 
 module Pwb
   RSpec.describe "Default theme page part content", type: :feature, js: false do
     context 'for contact-us page' do
-      let!(:pwb_page) { FactoryGirl.create(:contact_us_page_with_page_part)}
+      let!(:contact_us_page) { FactoryGirl.create(:page_with_content_html_page_part,
+                                                  slug: "contact-us") }
       # calling above :page would clash with page object
 
       let(:prop) { FactoryGirl.create(:pwb_prop, :sale) }
 
       scenario 'when general contact form is filled' do
-        page_content_html = pwb_page.contents.find_by_page_part_key "content_html"
+        page_content_html = contact_us_page.contents.find_by_page_part_key "content_html"
         page_content_html.raw = "Content html raw"
         page_content_html.save!
 
