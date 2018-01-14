46dc2c9792a64d51877937487c458ce8be90316f
Ed Tewiah <etewiah@hotmail.cim>
2018-01-14 20:46:07 +0100

    Ensure contact us page is rendered correctly for Berlin theme

--- spec/features/pwb/theme_berlin/page_part_content_spec.rb
+++ spec/features/pwb/theme_berlin/page_part_content_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 module Pwb
-  RSpec.feature "Default theme page part content", type: :feature, js: false do
+  RSpec.feature "Berlin theme page part content", type: :feature, js: false do
     feature 'for contact-us page' do
       let!(:contact_us_page) { FactoryGirl.create(:page_with_content_html_page_part,
                                                   slug: "contact-us") }
@@ -9,14 +9,21 @@ module Pwb
 
       let(:prop) { FactoryGirl.create(:pwb_prop, :sale) }
 
+
+      before(:all) do
+        @website = Website.unique_instance
+        @website.theme_name = "berlin"
+        @website.save!
+      end
+
+
       scenario 'correct content html is rendered' do
         page_content_html = contact_us_page.contents.find_by_page_part_key "content_html"
         page_content_html.raw = "Content html raw"
         page_content_html.save!
 
         visit('/contact-us')
-
-        expect(page).to have_css(".default-theme", :count => 1)
+        expect(page).to have_css(".berlin-theme", :count => 1)
         expect(page).to have_content 'Content html raw'
       end
     end
