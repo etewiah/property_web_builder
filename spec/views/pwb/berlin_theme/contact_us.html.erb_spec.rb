04146acd70546b5fcc21063a76edfbee0f0298c5
Ed Tewiah <etewiah@hotmail.cim>
2018-01-14 20:46:07 +0100

    Ensure contact us page is rendered correctly for Berlin theme

--- spec/views/pwb/berlin_theme/contact_us.html.erb_spec.rb
+++ spec/views/pwb/berlin_theme/contact_us.html.erb_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'pwb/sections/contact_us', type: :view do
   before do
     # @website = FactoryGirl.create(:pwb_website)
     view.extend Pwb::ApplicationHelper
+    view.extend Pwb::ComponentHelper
     # https://github.com/rspec/rspec-rails/issues/396
     # https://stackoverflow.com/questions/19282240/rspec-view-tests-cant-find-partials-that-are-in-base-namespace
     # can use below to test other views
@@ -17,8 +18,9 @@ RSpec.describe 'pwb/sections/contact_us', type: :view do
 
 
   before(:each) do
-    assign(:content_to_show, ["form_and_map"])
+    # assign(:content_to_show, ["form_and_map"])
     assign(:current_agency, FactoryGirl.create(:pwb_agency, company_name: 'my re'))
+    assign(:page, FactoryGirl.create(:contact_us_with_rails_page_part))
   end
 
   it 'renders contact-us form successfully' do
