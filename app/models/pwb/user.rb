959aef725baa77eb01681321bbe64f7dbab283f3
Dmitriy Borowskiy <d.borowskij@gmail.com>
2018-01-17 05:37:06 +0300

    find_for_oauth,  create_authorization

--- app/models/pwb/user.rb
+++ app/models/pwb/user.rb
@@ -11,5 +11,26 @@ module Pwb
     def default_client_locale
       :en
     end
+
+    def self.find_for_oauth(auth)
+      authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+      return authorization.user if authorization
+ 
+      email = auth.info[:email]
+      user = User.where(email: email).first
+      if user
+        user.create_authorization(auth)
+      else
+        password = Devise.friendly_token[0, 20]
+        user = User.create!(email: email, password: password, password_confirmation: password)
+        user.create_authorization(auth)
+      end
+ 
+      user
+    end
+ 
+    def create_authorization(auth)
+      self.authorizations.create(provider: auth.provider, uid: auth.uid)
+    end
   end
 end
