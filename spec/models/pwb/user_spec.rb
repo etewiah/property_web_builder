5f9a220bc64f5ab90671deed3f5b431303a4d30d
Dmitriy Borowskiy <d.borowskij@gmail.com>
2018-01-17 05:56:27 +0300

    tests rspec omniauth with facebook

--- spec/models/pwb/user_spec.rb
+++ spec/models/pwb/user_spec.rb
@@ -6,5 +6,71 @@ module Pwb
     it 'has a valid factory' do
       expect(user).to be_valid
     end
+
+    #tests authorization with omniauth
+    describe '.find_for_oauth' do
+      let!(:user) { create(:user) }
+      let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+ 
+      context 'user already has authorization' do
+        it 'returns the user' do
+          user.authorizations.create(provider: 'facebook', uid: '123456')
+          expect(User.find_for_oauth(auth)).to eq user
+        end
+      end
+ 
+    	context 'user has not authorization' do
+        context 'user already exists' do
+          let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: user.email }) }
+          it 'does not create new user' do
+            expect { User.find_for_oauth(auth) }.to_not change(User, :count)
+          end
+ 
+          it 'creates authorization for user' do
+            expect { User.find_for_oauth(auth) }.to change(user.authorizations, :count).by(1)
+          end
+ 
+          it 'creates authorization with provider and uid' do
+            authorization = User.find_for_oauth(auth).authorizations.first
+  
+            expect(authorization.provider).to eq auth.provider
+            expect(authorization.uid).to eq auth.uid
+          end
+  
+          it 'returns the user' do
+            expect(User.find_for_oauth(auth)).to eq user
+          end
+        end
+  
+        context 'user does not exist' do
+          let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: 'new@user.com' }) }
+  
+          it 'creates new user' do
+            expect { User.find_for_oauth(auth) }.to change(User, :count).by(1)
+          end
+          
+          it 'returns new user' do
+            expect(User.find_for_oauth(auth)).to be_a(User)
+          end
+  
+          it 'fills user email' do
+            user = User.find_for_oauth(auth)
+            expect(user.email).to eq auth.info[:email]
+          end
+  
+          it 'creates authorization for user' do
+            user = User.find_for_oauth(auth)
+            expect(user.authorizations).to_not be_empty
+          end
+  
+          it 'creates authorization with provider and uid' do
+            authorization = User.find_for_oauth(auth).authorizations.first
+  
+            expect(authorization.provider).to eq auth.provider
+            expect(authorization.uid).to eq auth.uid
+          end
+        end
+      end
+    end
   end
 end
