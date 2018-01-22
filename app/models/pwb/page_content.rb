c9ec6da42dd1a6a62e82b8fe2c65dd72aa0b0435
Ed Tewiah <etewiah@hotmail.cim>
2018-01-22 09:23:55 +0100

    For page_content model validate content_id_not_changed and page_part_key present

--- app/models/pwb/page_content.rb
+++ app/models/pwb/page_content.rb
@@ -1,12 +1,24 @@
 module Pwb
   class PageContent < ApplicationRecord
-    # join model for retrieving content for page
+    # Join model for creating content for page
+    # Would have been easier in the short run to have pages simply belong_to
+    # content directly.
+    # This join table provides flexibility in the long run to allow the
+    # same content to be used by different pages
+    # with different settings for sorting and visibility
     belongs_to :page
     belongs_to :content
     belongs_to :website
     # https://stackoverflow.com/questions/5856838/scope-with-join-on-has-many-through-association
 
-    # validates_presence_of :page_part_key
+
+    validate :content_id_not_changed
+
+
+
+
+    # page_part_key
+    validates_presence_of :page_part_key
 
     # this join model is used for sorting and visibility
     # (instead of a value on the content itself) as it
@@ -38,5 +50,17 @@ module Pwb
       content.present? ? content.page_part_key : ""
     end
 
+
+    private
+
+    def content_id_not_changed
+      if content_id_changed? && self.persisted?
+        if content_id_was.blank?
+          return
+        else
+          errors.add(:content_id, "Change of content_id not allowed!")
+        end
+      end
+    end
   end
 end
