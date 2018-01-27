2c77b7c24bf22c121d9b46cc620cab90d9004d05
Ed Tewiah <etewiah@hotmail.cim>
2018-01-27 16:15:10 +0100

    Ensure rails parts are seeded correctly

--- lib/pwb/contents_seeder.rb
+++ lib/pwb/contents_seeder.rb
@@ -37,40 +37,6 @@ module Pwb
 
           page_part_manager = Pwb::PagePartManager.new page_part_key, current_website
           set_page_part_content yml, page_part_key, page_part_manager, "website", locale
-
-
-          # page_part_key = page_part.page_part_key
-
-          # page_part_manager = Pwb::PagePartManager.new page_part_key, current_website
-
-          # unless yml[locale] && yml[locale]["website"] && yml[locale]["website"][page_part_key]
-          #   p "no content for website.... #{page_part_key} #{locale}."
-          #   # where there is no content to populate
-          #   # check if this is a rails_part (content is saved in a rails template)
-          #   if page_part.is_rails_part
-          #     # current_website.page_contents.find_or_create_by(page_part_key: page_part_key)
-
-          #     page_fragment_content = page_part_manager.find_or_create_content
-          #     # current_website.contents.find_or_create_by(page_part_key: page_part_key)
-          #     page_content_join_model = page_part_manager.get_join_model current_website
-          #     # page_fragment_content.page_contents.find_by_page_id current_website.id
-
-          #     page_content_join_model.page_part_key = page_part_key
-          #     # set is_rails_part on join_model too
-          #     page_content_join_model.is_rails_part = true
-          #     page_content_join_model.save!
-
-          #     # return
-          #   end
-          #   # skip if there is no content to populate
-          #   next
-          # end
-          # next unless yml[locale]["website"][page_part_key]
-          # seed_content = yml[locale]["website"][page_part_key]
-          # page_part_manager.seed_container_block_content locale, seed_content
-          # p "website #{page_part_key} content set for #{locale}."
-
-          # set_page_content_order_and_visibility locale, page_part, seed_content
         end
 
         Pwb::Page.all.each do |page|
@@ -78,6 +44,10 @@ module Pwb
             page_part_key = page_part.page_part_key
             page_part_manager = Pwb::PagePartManager.new page_part_key, page
 
+            # if page_part.is_rails_part
+            #   page_part_manager.find_or_create_join_model
+            # else
+            # end
             set_page_part_content yml, page_part_key, page_part_manager, page.slug, locale
             page_part_manager.set_default_page_content_order_and_visibility
 
@@ -85,6 +55,7 @@ module Pwb
         end
       end
 
+      # reads content from
       def set_page_part_content yml, page_part_key, page_part_manager, container_label, locale
 
         # Items in each locale seed file are nested as
@@ -95,83 +66,15 @@ module Pwb
           page_part_manager.seed_container_block_content locale, seed_content
           p "#{container_label} #{page_part_key} content set for #{locale}."
         else
+          # Might get here because the page_part is_rails_part or 
+          # does not have any content to be seeded.
+          # Still want to have a page_content entry based on that 
+          # page_part though
+          page_part_manager.find_or_create_join_model
           p "no content for #{container_label} page #{page_part_key} #{locale}."
-          # where there is no content to populate
-          # check if this is a rails_part (content is saved in a rails template)
-          # if page_part.is_rails_part
-          #   page_fragment_content = page_part_manager.find_or_create_content
-          #   page_content_join_model = page_fragment_content.page_contents.find_by_page_id page.id
-          #   page_content_join_model.page_part_key = page_part_key
-          #   # set is_rails_part on join_model too
-          #   page_content_join_model.is_rails_part = true
-          #   page_content_join_model.save!
-          #   # return
-          # end
-          # skip if there is no content to populate
-          # next
         end
-
       end
 
-      # def set_page_content_order_and_visibility(page_part)
-      #   page_part_editor_setup = page_part.editor_setup
-      #   page = page_part.page
-      #   # page_part_key uniquely identifies a fragment
-      #   page_part_key = page_part.page_part_key
-
-      #   sort_order = page_part_editor_setup["default_sort_order"] || 1
-      #   page.set_fragment_sort_order page_part_key, sort_order
-
-      #   visible_on_page = false
-      #   if page_part_editor_setup["default_visible_on_page"]
-      #     visible_on_page = true
-      #   end
-      #   page.set_fragment_visibility page_part_key, visible_on_page
-      # end
-
-      # def set_page_block_content(locale, page_part, seed_content)
-      #   page_part_editor_setup = page_part.editor_setup
-      #   page = page_part.page
-      #   # page_part_key uniquely identifies a fragment
-      #   page_part_key = page_part.page_part_key
-
-      #   # container for json to be attached to page details
-      #   locale_block_content_json = {"blocks" => {}}
-      #   # {"blocks"=>{"title_a"=>{"content"=>"about our agency"}, "content_a"=>{"content"=>""}}}
-      #   page_part_editor_setup["editorBlocks"].each do |configColBlocks|
-      #     configColBlocks.each do |configRowBlock|
-      #       row_block_label = configRowBlock["label"]
-      #       row_block_content = ""
-      #       # find the content for current block from within the seed content
-      #       if seed_content[row_block_label]
-      #         if configRowBlock["isImage"]
-      #           photo = page.seed_fragment_photo page_part_key, row_block_label, seed_content[row_block_label]
-      #           if photo.present? && photo.optimized_image_url.present?
-      #             # optimized_image_url is defined in content_photo and will
-      #             # return cloudinary url or filesystem url depending on settings
-      #             row_block_content = photo.optimized_image_url
-      #           else
-      #             row_block_content = "http://via.placeholder.com/350x250"
-      #           end
-      #         else
-      #           row_block_content = seed_content[row_block_label]
-      #         end
-      #       end
-      #       locale_block_content_json["blocks"][row_block_label] = {"content" => row_block_content}
-      #     end
-      #   end
-
-      #   # # save the block contents (in associated page_part model)
-      #   # updated_details = page.set_page_part_block_contents page_part_key, locale, locale_block_content_json
-      #   # # retrieve the contents saved above and use to rebuild html for that page_part
-      #   # # (and save it in associated page_content model)
-      #   # fragment_html = page.rebuild_page_content page_part_key, locale
-
-      #   result = page.update_page_part_content page_part_key, locale, locale_block_content_json
-
-
-      #   p "#{page.slug} page #{page_part_key} content set for #{locale}."
-      # end
     end
   end
 end
