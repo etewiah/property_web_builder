0ec4c4f0454526aefb7e5dfbd5cfc0d7f1f5ba67
Ed Tewiah <etewiah@hotmail.cim>
2018-02-19 16:21:50 +0100

    Enable Korean language

--- config/initializers/i18n_globalise.rb
+++ config/initializers/i18n_globalise.rb
@@ -1,6 +1,6 @@
 I18n.available_locales = [:ar, :ca, :de, :en, :es,
                           :fr, :it, :nl, :pl, :pt,
-                          :ro, :ru, :tr, :vi ]
+                          :ro, :ru, :tr, :vi, :ko ]
 # I18n.available_locales = [:en, :es, :fr, :de, :ru, :pt]
 
-Globalize.fallbacks = {de: [:en],es: [:en], pl: [:en], ro: [:en], ru: [:en]}
+Globalize.fallbacks = {de: [:en],es: [:en], pl: [:en], ro: [:en], ru: [:en], ko: [:en]}
