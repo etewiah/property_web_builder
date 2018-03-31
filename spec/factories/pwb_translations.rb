e35cce09f7416417e288069204083cbacd6c8b18
Ed Tewiah <etewiah@hotmail.cim>
2018-03-31 19:24:30 +0200

    Use correct class for translation spec

--- spec/factories/pwb_translations.rb
+++ spec/factories/pwb_translations.rb
@@ -1,4 +1,4 @@
 FactoryGirl.define do
-  factory :pwb_translation, class: 'Translation' do
+  factory :pwb_translation, class: 'I18n::Backend::ActiveRecord::Translation' do
   end
 end
--- spec/models/pwb/translation_spec.rb
+++ spec/models/pwb/translation_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
-module Pwb
-  RSpec.describe Translation, type: :model do
+module I18n::Backend
+  RSpec.describe ActiveRecord::Translation, type: :model do
+    # pending "add some examples to (or delete) #{__FILE__}"
     let(:translation) { FactoryGirl.create(:pwb_translation) }
     it 'has a valid factory' do
       expect(translation).to be_valid
