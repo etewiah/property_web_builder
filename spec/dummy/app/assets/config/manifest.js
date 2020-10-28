623c6d4967f9c1fdd0a418b4529f2afc04457751
Ed Tee <etewiah@gmail.com>
2020-10-28 13:07:35 +0300

    Add pwb_admin_panel/application to manifest

--- spec/dummy/app/assets/config/manifest.js
+++ spec/dummy/app/assets/config/manifest.js
@@ -3,3 +3,5 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 //= link pwb_manifest.js
+//= link pwb_admin_panel/application.css
+//= link pwb_admin_panel/application.js
