682ae45f41fe2f6bafe2b9387d1d35ca9f9886b5
Ed Tewiah <etewiah@hotmail.com>
2022-06-16 17:33:38 +0100

    Replace Pwb::Engine with Rails in lib directory

--- lib/pwb/pages_seeder.rb
+++ lib/pwb/pages_seeder.rb
@@ -1,5 +1,5 @@
 # To reload from console:
-# load "#{Pwb::Engine.root}/lib/pwb/pages_seeder.rb"
+# load "#{Rails.root}/lib/pwb/pages_seeder.rb"
 # Pwb::PagesSeeder.seed_page_content_translations!
 module Pwb
   class PagesSeeder
@@ -8,7 +8,7 @@ module Pwb
       # rake app:pwb:db:seed_pages
       # sets model entry for each page_part but not actual content
       def seed_page_parts!
-        page_parts_dir = Pwb::Engine.root.join('db', 'yml_seeds', 'page_parts')
+        page_parts_dir = Rails.root.join("db", "yml_seeds", "page_parts")
 
         page_parts_dir.children.each do |file|
           if file.extname == ".yml"
@@ -24,7 +24,7 @@ module Pwb
         page_yml_filenames = [
           "sell.yml", "about.yml", "buy.yml",
           "rent.yml", "home.yml", "legal_notice.yml",
-          "contact.yml", "privacy_policy.yml"
+          "contact.yml", "privacy_policy.yml",
         ]
 
         page_yml_filenames.each do |page_yml_filename|
@@ -34,32 +34,30 @@ module Pwb
 
       protected
 
-
       def seed_page(yml_file)
-        page_seed_file = Pwb::Engine.root.join('db', 'yml_seeds', 'pages', yml_file)
+        page_seed_file = Rails.root.join("db", "yml_seeds", "pages", yml_file)
         page_yml = YAML.load_file(page_seed_file)
         # unless Pwb::Page.where(slug: page_yml[0]['slug']).count > 0
         #   Pwb::Page.create!(page_yml)
         # end
 
-        page_record = Pwb::Page.find_by_slug(page_yml[0]['slug'])
+        page_record = Pwb::Page.find_by_slug(page_yml[0]["slug"])
         # unless Pwb::Page.where(slug: page_yml[0]['slug']).count > 0
         unless page_record.present?
           page_record = Pwb::Page.create!(page_yml[0])
         end
 
-
         # below sets the page title text from I18n translations
         # because setting the value in each page yml for each language
         # is not feasible
         I18n.available_locales.each do |locale|
-          title_accessor = 'page_title_' + locale.to_s
+          title_accessor = "page_title_" + locale.to_s
           # if page_title has not been set for this locale
           next unless page_record.send(title_accessor).blank?
-          translation_key = 'navbar.' + page_record.slug
+          translation_key = "navbar." + page_record.slug
           # get the I18n translation
           title_value = I18n.t(translation_key, locale: locale, default: nil)
-          title_value ||= I18n.t(translation_key, locale: :en, default: 'Unknown')
+          title_value ||= I18n.t(translation_key, locale: :en, default: "Unknown")
           # in case translation cannot be found
           # take default page_title (English value)
           title_value ||= page_record.page_title
@@ -76,7 +74,6 @@ module Pwb
         #   Pwb::PagePart.create!(yml_file_contents)
         # end
       end
-
     end
   end
 end
--- lib/pwb/seeder.rb
+++ lib/pwb/seeder.rb
@@ -1,5 +1,5 @@
 # To reload from console:
-# load "#{Pwb::Engine.root}/lib/pwb/seeder.rb"
+# load "#{Rails.root}/lib/pwb/seeder.rb"
 module Pwb
   class Seeder
     class << self
@@ -9,23 +9,23 @@ module Pwb
       def seed!
         I18n.locale = :en
         # unless ENV["RAILS_ENV"] == "test"
-        #   load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations.rb')
+        #   load File.join(Rails.root, 'db', 'seeds', 'translations.rb')
         # end
         unless I18n::Backend::ActiveRecord::Translation.all.length > 600
           # TODO: look in a directory and load all the files there
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_ca.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_en.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_es.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_de.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_fr.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_it.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_nl.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_pl.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_pt.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_ro.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_ru.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_ko.rb')
-          load File.join(Pwb::Engine.root, 'db', 'seeds', 'translations_bg.rb')
+          load File.join(Rails.root, "db", "seeds", "translations_ca.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_en.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_es.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_de.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_fr.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_it.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_nl.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_pl.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_pt.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_ro.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_ru.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_ko.rb")
+          load File.join(Rails.root, "db", "seeds", "translations_bg.rb")
         end
 
         # seed_sections 'sections.yml'
@@ -35,24 +35,24 @@ module Pwb
         # seed_content 'static.yml'
         # seed_content 'footer.yml'
         # seed_content 'sell.yml'
-        seed_agency 'agency.yml'
+        seed_agency "agency.yml"
         # need to seed website first so correct currency is used
-        seed_website 'website.yml'
+        seed_website "website.yml"
         # currency passed in for properties is ignored in favour
         # of default website currency
         unless Pwb::Prop.count > 3
-          seed_prop 'villa_for_sale.yml'
-          seed_prop 'villa_for_rent.yml'
-          seed_prop 'flat_for_sale.yml'
-          seed_prop 'flat_for_rent.yml'
-          seed_prop 'flat_for_sale_2.yml'
-          seed_prop 'flat_for_rent_2.yml'
+          seed_prop "villa_for_sale.yml"
+          seed_prop "villa_for_rent.yml"
+          seed_prop "flat_for_sale.yml"
+          seed_prop "flat_for_rent.yml"
+          seed_prop "flat_for_sale_2.yml"
+          seed_prop "flat_for_rent_2.yml"
         end
-        seed_field_keys 'field_keys.yml'
-        seed_users 'users.yml'
-        seed_contacts 'contacts.yml'
+        seed_field_keys "field_keys.yml"
+        seed_users "users.yml"
+        seed_contacts "contacts.yml"
         # seed_pages
-        seed_links 'links.yml'
+        seed_links "links.yml"
       end
 
       protected
@@ -60,7 +60,7 @@ module Pwb
       def seed_contacts(yml_file)
         contacts_yml = load_seed_yml yml_file
         contacts_yml.each do |contact_yml|
-          unless Pwb::Contact.where(primary_email: contact_yml['email']).count > 0
+          unless Pwb::Contact.where(primary_email: contact_yml["email"]).count > 0
             Pwb::Contact.create!(contact_yml)
           end
         end
@@ -69,7 +69,7 @@ module Pwb
       def seed_users(yml_file)
         users_yml = load_seed_yml yml_file
         users_yml.each do |user_yml|
-          unless Pwb::User.where(email: user_yml['email']).count > 0
+          unless Pwb::User.where(email: user_yml["email"]).count > 0
             Pwb::User.create!(user_yml)
           end
         end
@@ -78,7 +78,7 @@ module Pwb
       def seed_field_keys(yml_file)
         field_keys_yml = load_seed_yml yml_file
         field_keys_yml.each do |field_key_yml|
-          unless Pwb::FieldKey.where(global_key: field_key_yml['global_key']).count > 0
+          unless Pwb::FieldKey.where(global_key: field_key_yml["global_key"]).count > 0
             Pwb::FieldKey.create!(field_key_yml)
           end
         end
@@ -87,7 +87,7 @@ module Pwb
       def seed_links(yml_file)
         links_yml = load_seed_yml yml_file
         links_yml.each do |single_link_yml|
-          link_record = Pwb::Link.find_by_slug(single_link_yml['slug'])
+          link_record = Pwb::Link.find_by_slug(single_link_yml["slug"])
           # unless Pwb::Link.where(slug: single_link_yml['slug']).count > 0
           unless link_record.present?
             link_record = Pwb::Link.create!(single_link_yml)
@@ -97,15 +97,15 @@ module Pwb
           # because setting the value in links.yml for each language
           # is not feasible
           I18n.available_locales.each do |locale|
-            title_accessor = 'link_title_' + locale.to_s
+            title_accessor = "link_title_" + locale.to_s
             # if link_title has not been set for this locale
             next unless link_record.send(title_accessor).blank?
             # if link is associated with a page
-            if single_link_yml['page_slug']
-              translation_key = 'navbar.' + single_link_yml['page_slug']
+            if single_link_yml["page_slug"]
+              translation_key = "navbar." + single_link_yml["page_slug"]
               # get the I18n translation
               title_value = I18n.t(translation_key, locale: locale, default: nil)
-              title_value ||= I18n.t(translation_key, locale: :en, default: 'Unknown')
+              title_value ||= I18n.t(translation_key, locale: :en, default: "Unknown")
             end
             # in case translation cannot be found or link not associated with a page
             # take default link_title (English value)
@@ -129,7 +129,7 @@ module Pwb
         agency = Pwb::Agency.unique_instance
         unless agency.display_name.present?
           agency.update!(agency_yml)
-          agency_address_yml = load_seed_yml 'agency_address.yml'
+          agency_address_yml = load_seed_yml "agency_address.yml"
           agency_address = Pwb::Address.create!(agency_address_yml)
           agency.primary_address = agency_address
           agency.save!
@@ -137,10 +137,10 @@ module Pwb
       end
 
       def seed_prop(yml_file)
-        prop_seed_file = Pwb::Engine.root.join('db', 'yml_seeds', 'prop', yml_file)
+        prop_seed_file = Rails.root.join("db", "yml_seeds", "prop", yml_file)
         prop_yml = YAML.load_file(prop_seed_file)
         prop_yml.each do |single_prop_yml|
-          next if Pwb::Prop.where(reference: single_prop_yml['reference']).count > 0
+          next if Pwb::Prop.where(reference: single_prop_yml["reference"]).count > 0
           photos = []
           if single_prop_yml["photo_urls"].present?
             photos = create_photos_from_urls single_prop_yml["photo_urls"], Pwb::PropPhoto
@@ -159,7 +159,7 @@ module Pwb
       end
 
       # def seed_content(yml_file)
-      #   content_seed_file = Pwb::Engine.root.join('db', 'yml_seeds', yml_file)
+      #   content_seed_file = Rails.root.join('db', 'yml_seeds', yml_file)
       #   content_yml = YAML.load_file(content_seed_file)
       #   # tag is used to group content for an admin page
       #   # key is camelcase (js style) - used client side to identify each item in a group of content
@@ -192,8 +192,8 @@ module Pwb
         end
         photo_files.each do |photo_file|
           begin
-            photo = photo_class.send('create')
-            photo.image = Pwb::Engine.root.join(photo_file).open
+            photo = photo_class.send("create")
+            photo.image = Rails.root.join(photo_file).open
             photo.save!
             photos.push photo
           rescue Exception => e
@@ -215,7 +215,7 @@ module Pwb
         end
         photo_urls.each do |photo_url|
           begin
-            photo = photo_class.send('create')
+            photo = photo_class.send("create")
             photo.remote_image_url = photo_url
             photo.save!
             photos.push photo
@@ -229,7 +229,7 @@ module Pwb
       end
 
       def load_seed_yml(yml_file)
-        seed_file = Pwb::Engine.root.join('db', 'yml_seeds', yml_file)
+        seed_file = Rails.root.join("db", "yml_seeds", yml_file)
         yml = YAML.load_file(seed_file)
       end
     end
