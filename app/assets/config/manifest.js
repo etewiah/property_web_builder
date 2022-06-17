39ddb44a105248b1231562042f300bbd38ab7861
Ed Tewiah <etewiah@hotmail.com>
2022-06-17 10:56:25 +0100

    Declare js and css files in manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,9 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link pwb/themes/default.css
+//= link pwb/themes/berlin.css
+//= link pwb/themes/berlin.js
+//= link pwb/application.js
+//= link pwb_admin_panel/application.css
+//= link pwb_admin_panel/application.js
