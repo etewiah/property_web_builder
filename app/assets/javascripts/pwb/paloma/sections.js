4024c0a42399a952f587581469bdce9ddbe49426
Ed Tewiah <etewiah@hotmail.com>
2022-06-18 13:30:30 +0100

    Use gmaps api key in admin js too

--- app/assets/javascripts/pwb/paloma/sections.js
+++ app/assets/javascripts/pwb/paloma/sections.js
@@ -1,21 +1,4 @@
 var PwbSectionsController = Paloma.controller('Pwb/Sections');
 
-PwbSectionsController.prototype.contact_us = function() {
-
-  // var currentItemForMap = this.params.current_agency_primary_address;
-  // // http://stackoverflow.com/questions/2647867/how-to-determine-if-variable-is-undefined-or-null
-  // // if (currentItemForMap == null) {
-  // if (!this.params.show_contact_map) {
-  //   return;
-  // }
-  // if (typeof google === "undefined") {
-  //   var self = this;
-  //   window.map_callback = function() {
-  //     INMOAPP.showMap(currentItemForMap);
-  //   };
-  //   $.getScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyCPorm8YzIaUGhKfe5cvpgofZ_gdT8hdZw&v=3.exp&sensor=false&callback=map_callback&libraries=places');
-  // } else {
-  //   INMOAPP.showMap(currentItemForMap);
-  // }
-};
+PwbSectionsController.prototype.contact_us = function () { };
 
