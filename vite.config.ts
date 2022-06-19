03812adbe9ff5b9b8e62405593add3e80e4181bc
Ed Tewiah <etewiah@hotmail.com>
2022-06-19 15:56:33 +0100

    Initial hello world vue 3 page

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import VuePlugin from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    VuePlugin(),
   ],
 })
