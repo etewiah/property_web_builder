17c6a5f8a25b3aa33ba33887bb55dc391e2113ff
Ed Tewiah <etewiah@hotmail.com>
2022-06-19 22:55:45 +0100

    Add quaser package and import into vite

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -30,6 +30,14 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 // import '~/styles/background.css'
 import { createApp } from 'vue'
 import App from '~/v-admin-app/src/App.vue'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 
 createApp(App).mount('#app')
 
