051587ce21ca43dd74681e2f3e92ac233e15d560
Ed Tewiah <etewiah@hotmail.com>
2022-06-19 22:55:45 +0100

    Add quaser package and import into vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import VuePlugin from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    VuePlugin(),
+    VuePlugin({
+      template: { transformAssetUrls }
+    }),
+    quasar({
+      // sassVariables: 'src/quasar-variables.sass'
+    })
   ],
 })
