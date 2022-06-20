530ae9da17acf305d749dc97f88e2c9305663d0d
Ed Tewiah <etewiah@hotmail.com>
2022-06-20 14:13:37 +0100

    Add v-admin page and layout

---
+++ app/frontend/v-admin-app/src/layouts/MainLayout.vue
@@ -0,0 +1,198 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="toggleLeftDrawer"
+          icon="menu"
+          aria-label="Menu"
+        />
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-space />
+        <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn
+            round
+            dense
+            flat
+            color="white"
+            :icon="$q.fullscreen.isActive ? 'fullscreen_exit' : 'fullscreen'"
+            @click="$q.fullscreen.toggle()"
+            v-if="$q.screen.gt.sm"
+          >
+          </q-btn>
+          <q-btn
+            round
+            dense
+            flat
+            color="white"
+            icon="fab fa-github"
+            type="a"
+            href="/"
+            target="_blank"
+          >
+          </q-btn>
+          <q-btn
+            round
+            dense
+            flat
+            icon="fas fa-heart"
+            style="color: #9d4182 !important"
+            type="a"
+            href="/"
+            target="_blank"
+          >
+          </q-btn>
+          <q-btn round dense flat color="white" icon="notifications">
+            <q-badge color="red" text-color="white" floating> 5 </q-badge>
+            <q-menu>
+              <q-list style="min-width: 100px">
+                <!-- <messages></messages> -->
+                <q-card class="text-center no-shadow no-border">
+                  <q-btn
+                    label="View All"
+                    style="max-width: 120px !important"
+                    flat
+                    dense
+                    class="text-indigo-8"
+                  ></q-btn>
+                </q-card>
+              </q-list>
+            </q-menu>
+          </q-btn>
+          <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-primary text-white"
+    >
+      <q-list>
+        <q-item to="/" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="dashboard" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Dashboard</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item to="/Dashboard2" active-class="q-item-no-link-highlighting">
+          <q-item-section avatar>
+            <q-icon name="dashboard" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>CRM Dashboard</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-expansion-item
+          :default-opened="true"
+          aria-expanded="true"
+          icon="pages"
+          label="Properties"
+        >
+          <q-list class="q-pl-lg">
+            <q-item
+              :exact="true"
+              :to="{ name: 'rPropertiesList' }"
+              active-class="q-item-no-link-highlighting"
+            >
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>List All</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/Lock" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="lock" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Lock Screen</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/Lock-2" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="lock" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Lock Screen - 2</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item to="/Pricing" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Pricing</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item-label header class="text-weight-bolder text-white"
+              >Generic</q-item-label
+            >
+            <q-item to="/Profile" active-class="q-item-no-link-highlighting">
+              <q-item-section avatar>
+                <q-icon name="person" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>User Profile</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item
+              to="/Maintenance"
+              active-class="q-item-no-link-highlighting"
+            >
+              <q-item-section avatar>
+                <q-icon name="settings" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Maintenance</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-expansion-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container class="bg-grey-2">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+// import EssentialLink from 'components/EssentialLink.vue'
+// import Messages from "./Messages";
+
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    // EssentialLink,
+    // Messages
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
