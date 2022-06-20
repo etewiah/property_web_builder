6b0c8346b4ad3e8a387d58426b84b5a88711674b
Ed Tewiah <etewiah@hotmail.com>
2022-06-20 23:10:18 +0100

    Use q-table to display properties list

--- app/frontend/v-admin-app/src/pages/PropertiesList.vue
+++ app/frontend/v-admin-app/src/pages/PropertiesList.vue
@@ -1,15 +1,89 @@
 <template>
   <div>
-    <h1>properties...</h1>
-    <div v-for="property in properties" :key="property.id">
+    <!-- <div v-for="property in properties" :key="property.id">
       <div>{{ property.attributes.reference }}</div>
+    </div> -->
+    <div class="q-pa-md">
+      <q-table
+        title="All properties"
+        :rows="properties"
+        :columns="columns"
+        row-key="name"
+      >
+        <template v-slot:body="props">
+          <q-tr @click="goToProp(props.row)" :props="props">
+            <q-td key="ref" :props="props">
+              {{ props.row.attributes.reference }}
+            </q-td>
+            <q-td key="beds" :props="props">
+              {{ props.row.attributes["count-bedrooms"] }}
+            </q-td>
+            <q-td key="baths" :props="props">
+              {{ props.row.attributes["count-bathrooms"] }}
+            </q-td>
+            <q-td key="visible" :props="props">
+              {{ props.row.attributes["visible"] }}
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
     </div>
   </div>
 </template>
+
 <script>
-import useProperties from "../compose/useProperties.js";
+const columns = [
+  {
+    name: "ref",
+    required: true,
+    label: "Reference",
+    align: "left",
+    field: (row) => row.name,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "beds",
+    required: true,
+    label: "Bedrooms",
+    align: "left",
+    field: (row) => row.name,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "baths",
+    required: true,
+    label: "Bathrooms",
+    align: "left",
+    field: (row) => row.name,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "visible",
+    required: true,
+    label: "Visible",
+    align: "left",
+    field: (row) => row.name,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+]
+import useProperties from "../compose/useProperties.js"
 export default {
   components: {},
+  methods: {
+    goToProp(propertyRow) {
+      let targetRoute = {
+        name: "rPropertyEdit",
+        params: {
+          prop_id: propertyRow.id,
+        },
+      }
+      this.$router.push(targetRoute)
+    },
+  },
   mounted: function () {
     this.getProperties()
       .then((response) => {
@@ -20,16 +94,15 @@ export default {
   setup(props) {
     const { getProperties } = useProperties()
     return {
+      columns,
       getProperties,
-    };
+    }
   },
   data() {
     return {
-      // topListing: {},
-      // bottomListing: {},
       properties: [],
-    };
+    }
   },
-};
+}
 </script>
 <style></style>
