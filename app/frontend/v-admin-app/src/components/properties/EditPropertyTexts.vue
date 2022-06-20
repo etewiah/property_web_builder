3f34008e0c3bb143a3e03c61a06bea702f15241f
Ed Tewiah <etewiah@hotmail.com>
2022-06-20 23:54:35 +0100

    Add placeholder components for editing a property

---
+++ app/frontend/v-admin-app/src/components/properties/EditPropertyTexts.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="q-pa-md">
+      <q-card class="property-edit-card">
+        <q-card-section>
+          <div>Texts edit</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+<script>
+// import useProperties from "../compose/useProperties.js"
+export default {
+  components: {},
+  methods: {
+    // goToProp(propertyRow) {
+    //   let targetRoute = {}
+    //   this.$router.push(targetRoute)
+    // },
+  },
+  mounted: function () {
+    // this.getProperties()
+    //   .then((response) => {
+    //     this.properties = response.data.data
+    //   })
+    //   .catch((error) => {})
+  },
+  setup(props) {
+    // const { getProperties } = useProperties()
+    // return {
+    //   getProperties,
+    // }
+  },
+  data() {
+    return {
+      // currentProperty: {},
+      // activeTab: null,
+      // properties: [],
+    }
+  },
+}
+</script>
+<style></style>
