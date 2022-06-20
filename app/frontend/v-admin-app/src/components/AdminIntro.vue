0d1bb731b03b05124d414d2d5078eac232cbc547
Ed Tewiah <etewiah@hotmail.com>
2022-06-20 12:32:42 +0100

    Add vue router to v-admin app

---
+++ app/frontend/v-admin-app/src/components/AdminIntro.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Hellooooo</h1>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
       get "/config/:params" => "config#show"
 
       get "/v-admin" => "admin_panel_vue#show"
+      get "/v-admin/:params" => "admin_panel_vue#show"
     end
 
     get "/custom_css/:theme_name" => "css#custom_css", as: "custom_css"
