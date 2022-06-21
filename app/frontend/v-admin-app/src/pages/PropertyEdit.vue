5e22e596c535c9ed39669fe42ffa2ce12a40da03
Ed Tewiah <etewiah@hotmail.com>
2022-06-21 18:22:02 +0100

    Pass csrf token from document meta when updating a property

--- app/frontend/v-admin-app/src/pages/PropertyEdit.vue
+++ app/frontend/v-admin-app/src/pages/PropertyEdit.vue
@@ -73,7 +73,6 @@ export default {
   mounted: function () {
     this.getProperty(this.$route.params.prop_id)
       .then((response) => {
-        // debugger
         this.currentProperty = response.data.data
       })
       .catch((error) => {})
