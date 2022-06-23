6f35b352acc5c76f1021a81c2be72769a2da1f42
Ed Tewiah <etewiah@hotmail.com>
2022-06-23 17:20:42 +0100

    Enable update of translation item

--- app/frontend/v-admin-app/src/components/editor-forms-parts/TranslationInput.vue
+++ app/frontend/v-admin-app/src/components/editor-forms-parts/TranslationInput.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <div>{{ fieldDetails.sortKey }}</div>
+    <div>{{ translationUnit.sortKey }}</div>
     <div>
       <div
-        v-for="translationInstance in fieldDetails.translationsForKey"
+        v-for="translationInstance in translationUnit.translationsForKey"
         :key="translationInstance.key"
       >
         <div v-if="['es', 'en'].includes(translationInstance.locale)">
@@ -17,40 +17,91 @@
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-12">
+        <TranslationSubmitter
+          :cancelPendingChanges="cancelPendingChanges"
+          :lastChangedField="lastChangedField"
+          @changesCanceled="changesCanceled"
+          @runModelUpdate="runModelUpdate"
+        ></TranslationSubmitter>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import useTranslations from "~/v-admin-app/src/compose/useTranslations.js"
+import TranslationSubmitter from "~/v-admin-app/src/components/editor-forms-parts/TranslationSubmitter.vue"
 import TextField from "~/v-admin-app/src/components/editor-forms-parts/TextField.vue"
 export default {
   components: {
+    TranslationSubmitter,
     TextField,
   },
   props: {
-    fieldDetails: {},
-    currentFieldValue: {},
-    cancelPendingChanges: {},
-    autofocus: {},
+    translationUnit: {},
   },
   data() {
     return {
-      // localFieldValue: "",
-      // originalValue: "",
+      cancelPendingChanges: false,
+      lastChangedField: {
+        fieldDetails: {},
+        // lastUpdateStamp: "",
+      },
     }
   },
-  // watch: {
-  //   cancelPendingChanges(newValue, oldValue) {
-  //     if (oldValue === false) {
-  //       this.localFieldValue = this.originalValue
-  //     }
-  //   },
-  // },
   computed: {},
+  setup(props) {
+    const { updateTranslations } = useTranslations()
+    return {
+      updateTranslations,
+    }
+  },
   methods: {
-    updatePendingChanges(newValue) {
-      this.$emit("updatePendingChanges", {
-        fieldDetails: this.fieldDetails,
-        newValue: newValue,
-      })
+    updatePendingChanges({ fieldDetails, newValue }) {
+      fieldDetails.newValue = newValue
+      fieldDetails.batch_key = "extras"
+      this.lastChangedField.fieldDetails = fieldDetails
+      // this.lastChangedField.lastUpdateStamp = Date.now()
+      this.cancelPendingChanges = false
+    },
+    changesCanceled() {
+      this.$emit("changesCanceled")
+      this.cancelPendingChanges = true
+    },
+    runModelUpdate(currPendingChanges) {
+      this.updateTranslations(currPendingChanges)
+      this.$q
+        .notify({
+          color: "green-4",
+          textColor: "white",
+          icon: "cloud_done",
+          message: "Updated successfully",
+        })
+
+        // .then((response) => {
+        //   this.$q.notify({
+        //     color: "green-4",
+        //     textColor: "white",
+        //     icon: "cloud_done",
+        //     message: "Updated successfully",
+        //   })
+        // })
+        // .catch((error) => {
+        //   let errorMessage = error.message || "Sorry, unable to update"
+        //   if (
+        //     error.response.data.errors[0] &&
+        //     error.response.data.errors[0].meta.exception
+        //   ) {
+        //     errorMessage = error.response.data.errors[0].meta.exception
+        //   }
+        //   this.$q.notify({
+        //     color: "red-4",
+        //     textColor: "white",
+        //     icon: "error",
+        //     message: errorMessage,
+        //   })
+        // })
     },
   },
 }
--- app/frontend/v-admin-app/src/compose/useTranslations.js
+++ app/frontend/v-admin-app/src/compose/useTranslations.js
@@ -10,21 +10,28 @@ export default function () {
       // }
     })
   }
-  function updateAgency(agency_changes) {
-    let apiUrl = `${dataApiBase}/api/v1/agency`
+  function updateTranslations(translation_changes) {
     let csrfToken = document.head.querySelector("[name='csrf-token']").content
-    return axios.put(apiUrl, {
-      agency: agency_changes
-    }, {
-      headers: {
-        // 'Content-Type': 'application/vnd.api+json',
-        'X-CSRF-Token': csrfToken
+    Object.keys(translation_changes).forEach(change_locale => {
+      let tc = translation_changes[change_locale]
+      let apiUrl = `${dataApiBase}/api/v1/translations/${tc.id}/update_for_locale`
+      let translation_change = {
+        i18n_value: tc.newValue,
+        id: tc.id
+        // batch_key: tc.batch_key
       }
-      // headers: authHeader()
+      debugger
+      axios.put(apiUrl, translation_change, {
+        headers: {
+          // 'Content-Type': 'application/vnd.api+json',
+          'X-CSRF-Token': csrfToken
+        }
+        // headers: authHeader()
+      })
     })
   }
   return {
-    updateAgency,
+    updateTranslations,
     getTranslations
   }
 }
