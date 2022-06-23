bf18b0ab41939eb8894812d2bdb8ade5649a8c4a
Ed Tewiah <etewiah@hotmail.com>
2022-06-23 15:22:36 +0100

    Add placeholder files for managing translations

--- app/frontend/v-admin-app/src/layouts/MainLayout.vue
+++ app/frontend/v-admin-app/src/layouts/MainLayout.vue
@@ -102,6 +102,18 @@
             <q-item-label>Agency</q-item-label>
           </q-item-section>
         </q-item>
+        <q-item
+          :exact="true"
+          :to="{ name: 'rTranslationsEditFeatures' }"
+          active-class="q-item-no-link-highlighting"
+        >
+          <q-item-section avatar>
+            <q-icon name="dashboard" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Translations</q-item-label>
+          </q-item-section>
+        </q-item>
         <!-- <q-item to="/Dashboard2" active-class="q-item-no-link-highlighting">
           <q-item-section avatar>
             <q-icon name="dashboard" />
