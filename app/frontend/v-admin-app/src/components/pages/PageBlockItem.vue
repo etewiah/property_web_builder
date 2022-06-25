1f759da7c3b10e7162a341451f78ed7ee874fcd2
Ed Tewiah <etewiah@hotmail.com>
2022-06-25 11:15:00 +0100

    Group edit fields for a page part together

---
+++ app/frontend/v-admin-app/src/components/pages/PageBlockItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div v-if="editorBlockItem.isHtml || editorBlockItem.isMultipleLineText">
+      <HtmlField
+        :cancelPendingChanges="cancelPendingChanges"
+        :fieldDetails="editorBlockItemFieldDetails"
+        :currentFieldValue="blockValue"
+        v-on:updatePendingChanges="updatePendingChanges"
+      ></HtmlField>
+    </div>
+    <div v-else-if="editorBlockItem.isSingleLineText">
+      <TextField
+        :cancelPendingChanges="cancelPendingChanges"
+        :fieldDetails="editorBlockItemFieldDetails"
+        :currentFieldValue="blockValue"
+        v-on:updatePendingChanges="updatePendingChanges"
+      ></TextField>
+    </div>
+    <div v-else>
+      {{ blockValue }}
+    </div>
+  </div>
+</template>
+<script>
+import TextField from "~/v-admin-app/src/components/editor-forms-parts/TextField.vue"
+import HtmlField from "~/v-admin-app/src/components/editor-forms-parts/HtmlField.vue"
+export default {
+  components: {
+    TextField,
+    HtmlField,
+  },
+  computed: {
+    blockValue() {
+      let block =
+        this.pagePartDetails.block_contents[this.editorLocale].blocks[
+          this.editorBlockItem.label
+        ] || {}
+      return block.content || ""
+    },
+    editorBlockItemFieldDetails() {
+      let qInputType = ""
+      if (this.editorBlockItem.isMultipleLineText) {
+        qInputType = "textarea"
+      }
+      if (this.editorBlockItem.isSingleLineText) {
+        qInputType = "text"
+      }
+      return {
+        qInputType: qInputType,
+        fieldName: this.editorBlockItem.label,
+        // "content", //
+      }
+    },
+  },
+  methods: {
+    updatePendingChanges({ fieldDetails, newValue }) {
+      this.$emit("updatePendingChanges", {
+        fieldDetails: fieldDetails,
+        newValue: newValue,
+      })
+    },
+  },
+  props: {
+    cancelPendingChanges: {
+      type: Boolean,
+      default: false,
+    },
+    pagePartDetails: {},
+    editorBlockItem: {},
+    editorLocale: {},
+  },
+  // setup(props) {},
+  data() {
+    return {}
+  },
+}
+</script>
+<style></style>
