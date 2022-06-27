cbf21630a8ec79bb6abbda0260818ce7bf80af97
Ed Tewiah <etewiah@hotmail.com>
2022-06-27 14:19:30 +0100

    Add .env.example

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import VuePlugin from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
+  // define: {
+  //   'process.env.GMAPS_API_KEY': JSON.stringify(process.env.GMAPS_API_KEY),
+  //   'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
+  // },
   plugins: [
     RubyPlugin(),
     VuePlugin({
