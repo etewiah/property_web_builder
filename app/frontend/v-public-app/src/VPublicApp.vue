c70bbd805dba878acd3a38b359c415391c3962cf
Ed Tewiah <etewiah@hotmail.com>
2022-06-29 18:21:12 +0100

    Add entrypoint and files for  v-public vue app

---
+++ app/frontend/v-public-app/src/VPublicApp.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from "vue"
+import { websiteProvider } from "~/v-admin-app/src/compose/website-provider.js"
+export default defineComponent({
+  name: "App",
+  provide: {
+    websiteProvider,
+  },
+})
+</script>
