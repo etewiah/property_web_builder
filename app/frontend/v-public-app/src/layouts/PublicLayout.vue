a1cbf51964f8b71b2a3466a1b03baecaba100ff1
Ed Tewiah <etewiah@hotmail.com>
2022-06-30 15:28:51 +0100

    POC of using urql to retrieve page parts from graphql endpoint

---
+++ app/frontend/v-public-app/src/layouts/PublicLayout.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          <strong>Property</strong><span style="color: black">Web</span
+          ><strong class="navy--text text--darken-1">Builder</strong>
+        </q-toolbar-title>
+        <q-space />
+        <div class="q-gutter-sm row items-center no-wrap">
+          <!-- <q-btn round flat>
+            <q-avatar size="26px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-btn> -->
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container class="bg-grey-2">
+      <div v-for="pageBlock in pageBlocks" :key="pageBlock.id">
+        <div v-html="pageBlock"></div>
+      </div>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+import { defineComponent, ref } from "vue"
+import { useQuery } from "@urql/vue"
+export default defineComponent({
+  name: "PublicLayout",
+  components: {},
+  computed: {
+    pageBlocks() {
+      let pageBlocks = []
+      if (this.data && this.data.findPage.pageParts) {
+        this.data.findPage.pageParts.forEach((pagePart) => {
+          if (
+            pagePart.blockContents["en"] &&
+            pagePart.blockContents["en"].page_part_key === "content_html"
+          ) {
+            let mainContentHtml =
+              pagePart.blockContents["en"].blocks.main_content.content
+            pageBlocks.push(mainContentHtml)
+          }
+        })
+      }
+      return pageBlocks
+    },
+    // supportedLocaleDetails() {
+    //   let supportedLocales = this.currentWebsite.supported_locales || []
+    //   let supportedLocaleDetails = []
+    //   supportedLocales.forEach((localeWithVariant) => {
+    //     let localeOnly = localeWithVariant.split("-")[0]
+    //     supportedLocaleDetails.push({
+    //       localeOnly: localeOnly,
+    //       localeWithVariant: localeWithVariant,
+    //       label: this.adminTranslations[localeOnly],
+    //     })
+    //   })
+    //   return supportedLocaleDetails
+    // },
+  },
+  mounted: function () {},
+  data() {
+    return {
+      // adminTranslations: {},
+      activeTab: null,
+    }
+  },
+  setup() {
+    const result = useQuery({
+      query: `
+        query {
+          findPage(slug: "home") {
+            rawHtml,
+            pageParts {
+              blockContents
+              createdAt
+              pageSlug
+            }
+          }
+        }
+      `,
+    })
+
+    return {
+      fetching: result.fetching,
+      data: result.data,
+      error: result.error,
+    }
+  },
+})
+</script>
