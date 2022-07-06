98e20b19008cec606091c452764ffb3b4151fc94
Ed Tewiah <etewiah@hotmail.com>
2022-07-06 14:57:18 +0100

    Add agency and website graphql types for retrieving site details

---
+++ app/graphql/types/website_type.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Types
+  class WebsiteType < Types::BaseObject
+    field :default_currency, String, null: true
+    field :social_media, String, null: true
+    field :phone_number_primary, String, null: true
+    field :phone_number_primary, String, null: true
+    field :phone_number_primary, String, null: true
+    field :phone_number_primary, String, null: true
+    field :social_media, GraphQL::Types::JSON, null: true
+
+    field :supported_locales, GraphQL::Types::JSON, null: true
+    field :supported_locales_with_variants, GraphQL::Types::JSON, null: true
+    field :style_variables, GraphQL::Types::JSON, null: true
+    field :top_nav_display_links, [Types::LinkType], null: true
+    field :footer_display_links, [Types::LinkType], null: true
+    field :agency, Types::AgencyType, null: false
+  end
+end
