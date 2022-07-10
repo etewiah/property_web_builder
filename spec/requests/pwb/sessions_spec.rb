4eac41a82671b39ddae90bfcb056dd472cc534c9
Ed Tewiah <etewiah@hotmail.com>
2022-07-11 00:53:51 +0100

    Fix pwb. prefix on paths in specs

--- spec/requests/pwb/sessions_spec.rb
+++ spec/requests/pwb/sessions_spec.rb
@@ -1,23 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
 module Pwb
   RSpec.describe "Sessions", type: :request do
     before(:all) do
-      @agency = Agency.last || FactoryBot.create(:pwb_agency, company_name: 'my re')
+      @agency = Agency.last || FactoryBot.create(:pwb_agency, company_name: "my re")
 
       @admin_user = User.create!(email: "user@example.org", password: "very-secret", admin: true)
     end
 
     it "signs user in and out" do
       sign_in @admin_user
-      get pwb.admin_path
+      get admin_path
       # byebug
       expect(controller.current_user).to eq(@admin_user)
 
       sign_out @admin_user
       get "/admin"
       # expect(controller.current_user).to be_nil
-      expect(response).to redirect_to(pwb.new_user_session_path)
+      expect(response).to redirect_to(new_user_session_path)
     end
 
     after(:all) do
