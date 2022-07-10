179750b14669034c586e03e305b2d8e711952bb8
Ed Tewiah <etewiah@hotmail.com>
2022-07-11 00:35:55 +0100

    Remove references to deprecated jsonapi-resources actions

--- app/controllers/pwb/api/v1/web_contents_controller.rb
+++ app/controllers/pwb/api/v1/web_contents_controller.rb
@@ -2,7 +2,7 @@ module Pwb
   class Api::V1::WebContentsController < JSONAPI::ResourceController
     # Skipping action below allows me to browse to endpoint
     # without having set mime type
-    skip_before_action :ensure_valid_accept_media_type
+    # skip_before_action :ensure_valid_accept_media_type
 
     # below is used by logo_photo and about_us_photo,
     # where only one photo is allowed
@@ -14,7 +14,7 @@ module Pwb
       unless photo
         if content_tag
           # where photo has never been set before, associated Content will not exist
-          content = Content.find_by_key(content_tag) || Content.create({ key: content_tag, tag: 'appearance' })
+          content = Content.find_by_key(content_tag) || Content.create({ key: content_tag, tag: "appearance" })
           photo = ContentPhoto.create
           if content_tag == "logo"
             # TODO: This is a workaround
