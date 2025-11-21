d79d256c2fc42106809556d16f36faf58bd4c750
Ed Tewiah <etewiah@hotmail.com>
2025-11-21 15:48:24 +0000

    Add pwb_admin_panel/application.scss

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -6,7 +6,9 @@ Rails.application.config.dartsass.builds = {
   "pwb/themes/default.scss" => "pwb/themes/default.css",
   "pwb/themes/matt.scss" => "pwb/themes/matt.css",
   "pwb/themes/squares.scss" => "pwb/themes/squares.css",
-  "pwb/themes/vic.scss" => "pwb/themes/vic.css"
+  "pwb/themes/squares.scss" => "pwb/themes/squares.css",
+  "pwb/themes/vic.scss" => "pwb/themes/vic.css",
+  "pwb_admin_panel/application.scss" => "pwb_admin_panel/application.css"
 }
 
 Rails.application.config.dartsass.build_options << " --load-path=app/assets/sass --load-path=vendor/assets/stylesheets"
