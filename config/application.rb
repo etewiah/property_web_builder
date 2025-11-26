d5bacf7ee5197e90b8433f9017371a2c5563289a
Ed Tewiah <etewiah@hotmail.com>
2025-11-26 12:25:48 +0000

    Ignore pwb/version.rb in Zeitwerk autoloading

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module StandalonePwb
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks pwb/version.rb])
     config.assets.css_compressor = nil
 
     # Configuration for the application, engines, and railties goes here.
