613a09db48d005ab2f976b16c36a020a2698c49c
Ed Tewiah <etewiah@hotmail.com>
2025-11-29 16:47:08 +0000

    Use leaflet instead of google maps for inmo-map

--- app/assets/javascripts/pwb/shared/inmo-map.js
+++ app/assets/javascripts/pwb/shared/inmo-map.js
@@ -1,121 +1,74 @@
 Vue.component('inmo-map', {
-  template: '<gmap-map style="min-height: 600px;"' +
-    ':zoom="15" :center="center" ref="mmm">' +
-    '<gmap-info-window ref="infwin" :options="infoOptions" :position="infoWindowPos"' +
-    ':opened="infoWinOpen" @closeclick="infoWinOpen=false">' +
-    '</gmap-info-window>' +
-    '<gmap-marker  v-for="(m,i) in mapkers"  :key="m.id" :position="m.position" ' +
-    '@mouseover="toggleInfoWindow(m,i)" @mouseout="statusText = null"' +
-    ':clickable="true" :draggable="true" @click=""></gmap-marker>' +
-    '</gmap-map>',
+  template: '<div id="inmo-map-canvas" style="min-height: 600px; width: 100%;"></div>',
   data: function() {
     return {
-      newMarkers: [],
-      useNewMarkers: false,
-      infoContent: '',
-      statusText: '',
-      infoWindowPos: {
-        lat: 0,
-        lng: 0
-      },
-      infoWinOpen: false,
-      currentMidx: null,
-      //optional: offset infowindow so it visually sits nicely on top of our marker
-      infoOptions: {
-        content: "hey",
-        pixelOffset: {
-          width: 0,
-          height: -35
-        }
-      }
+      map: null,
+      markersLayer: null,
+      internalMarkers: []
     };
   },
-  // created() {
-  // },
   mounted: function() {
-    var that = this;
-    this.$refs.mmm.$mapCreated.then(function() {
-      if (that.mapkers.length > 1) {
-        var bounds = new google.maps.LatLngBounds();
-        that.mapkers.forEach(function(mapker){
-          bounds.extend(mapker.position);
-        });
-        // for (let m of that.mapkers) {
-        //   bounds.extend(m.position)
-        // }
-        that.$refs.mmm.$mapObject.fitBounds(bounds);
-        // where markers are too close together, I need below
-        // to ensure they are not too zoomed in
-        that.$refs.mmm.$mapObject.setOptions({ maxZoom: that.$refs.mmm.$mapObject.getZoom() });
-      }
-    })
+    this.initMap();
   },
   methods: {
+    initMap: function() {
+      // Default center (will be updated by bounds)
+      this.map = L.map('inmo-map-canvas').setView([0, 0], 2);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(this.map);
+
+      this.markersLayer = L.layerGroup().addTo(this.map);
+
+      if (this.markers && this.markers.length > 0) {
+        this.addMarkers(this.markers);
+      }
+    },
+    addMarkers: function(markersData) {
+      this.markersLayer.clearLayers();
+      var bounds = L.latLngBounds();
+
+      markersData.forEach(function(markerData) {
+        var lat = markerData.position.lat;
+        var lng = markerData.position.lng;
+        
+        if (lat && lng) {
+          var marker = L.marker([lat, lng]);
+          
+          var popupContent = '<div id="iw-container">' +
+            '<a href="' + markerData.show_url + '">' +
+            '<div class="iw-title">' + markerData.title + '</div>';
+            
+          if (markerData.display_price) {
+            popupContent += '<div class="iw-subTitle">' + markerData.display_price + '</div>';
+          }
+          
+          if (markerData.image_url) {
+            popupContent += '<div class="iw-content"><img src="' + markerData.image_url + '" alt="" width="225"></div>';
+          }
+          
+          popupContent += '</a></div>';
+          
+          marker.bindPopup(popupContent);
+          this.markersLayer.addLayer(marker);
+          bounds.extend([lat, lng]);
+        }
+      }, this);
+
+      if (markersData.length > 0) {
+        this.map.fitBounds(bounds, { padding: [50, 50] });
+      }
+    },
     resetMarkers: function(newMarkers) {
-      this.newMarkers = newMarkers;
-      this.useNewMarkers = true;
-    },
-    toggleInfoWindow: function(marker, idx) {
-      this.infoWindowPos = marker.position;
-      var display_price_html = "";
-      if (marker.display_price) {
-        display_price_html = '<div class="iw-subTitle">' + marker.display_price + '</div>';
-      }
-      var image_html = "";
-      if (marker.image_url) {
-        image_html = '<img src="' + marker.image_url + '" alt="" width="225">';
-      }
-
-      var infoWindowContent = '<div id="iw-container">' +
-        '<a href="' + marker.show_url + '" class="">' +
-        '<div class="iw-title">' + marker.title + '</div>' +
-        '<div class="iw-content">' +
-        display_price_html +
-        image_html +
-        '</div></a>' +
-        '</div>';
-
-      this.infoOptions.content = infoWindowContent;
-      //check if its the same marker that was selected if yes toggle
-      if (this.currentMidx == idx) {
-        this.infoWinOpen = !this.infoWinOpen;
-      }
-      //if different marker set infowindow to open and reset current marker index
-      else {
-        this.infoWinOpen = true;
-        this.currentMidx = idx;
-      }
-    }
-  },
-  // watch: {
-  //   mapkers(mapkers) {
-  //   }
-  // },
-  computed: {
-    mapkers: function() {
-      if (this.useNewMarkers) {
-        return this.newMarkers;
-      } else {
-        return this.markers;
-      }
-    },
-    center: function() {
-      if (this.markers) {
-        var lat = this.markers[0].position.lat;
-        var lng = this.markers[0].position.lng;
-        return { lat: lat, lng: lng };
-
-        // if (this.mapkers.length < 2) {
-        // } else {
-        //   // const bounds = new google.maps.LatLngBounds()
-        //   // for (let m of mapkers) {
-        //   //   bounds.extend(m.latLng)
-        //   // }
-        //   // this.$refs.map.$mapObject.fitBounds(bounds)
-        // }
-      }
-      // `this` points to the vm instance
+      this.internalMarkers = newMarkers;
+      this.addMarkers(newMarkers);
     }
   },
   props: ['markers'],
+  watch: {
+    markers: function(newVal) {
+      this.addMarkers(newVal);
+    }
+  }
 });
