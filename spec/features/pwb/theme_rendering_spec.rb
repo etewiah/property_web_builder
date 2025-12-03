6e592e0e7d376d86a02cec540e64537399e35172
Ed Tewiah <etewiah@hotmail.com>
2025-12-03 12:26:19 +0000

    More improvements to page_parts to work better with tailwind

--- spec/features/pwb/theme_rendering_spec.rb
+++ spec/features/pwb/theme_rendering_spec.rb
@@ -33,6 +33,20 @@ module Pwb
       HTML
     end
 
+    let(:our_agency_template) do
+      <<~HTML
+        <section class="our-agency-section">
+          <div class="agency-container">
+            <div class="agency-content-wrapper">
+              <h3 class="agency-title">
+                <span>{{ page_part["title_a"]["content"] }}</span>
+              </h3>
+            </div>
+          </div>
+        </section>
+      HTML
+    end
+
     before do
       # Create PageParts with the new semantic templates
       hero_editor_setup = {
@@ -109,5 +123,47 @@ module Pwb
       expect(page).to have_css('.services-section-wrapper')
       expect(page).to have_css('.services-container')
     end
+
+    scenario 'About Us page renders with semantic CSS classes' do
+      about_us_page = FactoryBot.create(:pwb_page, slug: "about-us", website: website)
+
+      agency_editor_setup = {
+        "editorBlocks" => [
+          [
+            { "label" => "title_a", "isSingleLineText" => "true" },
+            { "label" => "content_a", "isMultipleLineText" => "true" },
+            { "label" => "our_agency_img", "isImage" => "true" }
+          ]
+        ]
+      }
+
+      agency_part = FactoryBot.create(:pwb_page_part,
+        page_part_key: "our_agency",
+        page_slug: "about-us",
+        template: our_agency_template,
+        editor_setup: agency_editor_setup,
+        website: website
+      )
+
+      agency_content_seed = {
+        "title_a" => "Our Agency Story",
+        "content_a" => "We started in 1990..."
+      }
+
+      agency_manager = Pwb::PagePartManager.new("our_agency", about_us_page)
+      agency_manager.seed_container_block_content("en", agency_content_seed)
+
+      unless about_us_page.page_parts.include?(agency_part)
+        about_us_page.page_parts << agency_part
+      end
+      about_us_page.page_contents.update_all(visible_on_page: true)
+
+      visit('/about-us')
+
+      expect(page).to have_css('.our-agency-section')
+      expect(page).to have_css('.agency-container')
+      expect(page).to have_css('.agency-title', text: 'Our Agency Story')
+    end
   end
 end
+
