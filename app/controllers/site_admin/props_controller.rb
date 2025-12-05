488b4c6a54405c36db9e68aed40f2a9688201eab
Ed Tewiah <etewiah@hotmail.com>
2025-12-05 08:22:29 +0000

    Update property editing in site_admin to work with new models

--- app/controllers/site_admin/props_controller.rb
+++ app/controllers/site_admin/props_controller.rb
@@ -14,7 +14,8 @@ module SiteAdmin
 
     def index
       # Use Pwb::ListedProperty (materialized view) for listing - it's optimized for reads
-      @props = Pwb::ListedProperty.order(created_at: :desc).limit(100)
+      # Scope to current website
+      @props = Pwb::ListedProperty.where(website_id: current_website&.id).order(created_at: :desc).limit(100)
 
       if params[:search].present?
         @props = @props.where('reference ILIKE ?', "%#{params[:search]}%")
@@ -30,6 +31,9 @@ module SiteAdmin
     end
 
     def edit_text
+      # Load listings for text editing - titles/descriptions are on listings now
+      @sale_listing = @prop.sale_listings.first
+      @rental_listing = @prop.rental_listings.first
       render :edit_text
     end
 
@@ -45,6 +49,22 @@ module SiteAdmin
     end
 
     def edit_features
+      # Load available features from FieldKey where tag is 'extras'
+      # If no field keys exist, provide a default set of common property features
+      @available_features = Pwb::FieldKey.where(tag: 'extras').visible.pluck(:global_key)
+
+      if @available_features.empty?
+        @available_features = %w[
+          extras.pool extras.garden extras.terrace extras.balcony
+          extras.air_conditioning extras.heating extras.elevator extras.storage
+          extras.security extras.video_intercom extras.gated_community extras.parking
+          extras.sea_view extras.mountain_view extras.fireplace extras.fitted_kitchen
+        ]
+      end
+
+      # Get current features for this property
+      @current_features = @prop.features.pluck(:feature_key)
+
       render :edit_features
     end
 
@@ -104,6 +124,11 @@ module SiteAdmin
           rental_listing = @prop.rental_listings.first_or_initialize
           rental_listing.update!(rental_listing_params)
         end
+
+        # Update features if features params present
+        if params[:features].present?
+          update_features(params[:features])
+        end
       end
 
       redirect_to site_admin_prop_path(@prop), notice: 'Property was successfully updated.'
@@ -115,12 +140,14 @@ module SiteAdmin
 
     def set_property
       # Use Property view for read-only show action
-      @prop = Pwb::ListedProperty.find(params[:id])
+      # Scope to current website for security
+      @prop = Pwb::ListedProperty.where(website_id: current_website&.id).find(params[:id])
     end
 
     def set_realty_asset
       # Use RealtyAsset for write operations
-      @prop = Pwb::RealtyAsset.find(params[:id])
+      # Scope to current website for security
+      @prop = Pwb::RealtyAsset.where(website_id: current_website&.id).find(params[:id])
     end
 
     def asset_params
@@ -141,22 +168,47 @@ module SiteAdmin
     def sale_listing_params
       return {} unless params[:sale_listing].present?
 
+      # Include title/description for each supported locale
+      locale_fields = I18n.available_locales.flat_map do |locale|
+        ["title_#{locale}".to_sym, "description_#{locale}".to_sym]
+      end
+
       params.require(:sale_listing).permit(
         :visible, :highlighted, :archived, :reserved, :furnished,
         :price_sale_current_cents, :price_sale_current_currency,
-        :commission_cents, :commission_currency
+        :commission_cents, :commission_currency,
+        *locale_fields
       )
     end
 
     def rental_listing_params
       return {} unless params[:rental_listing].present?
 
+      # Include title/description for each supported locale
+      locale_fields = I18n.available_locales.flat_map do |locale|
+        ["title_#{locale}".to_sym, "description_#{locale}".to_sym]
+      end
+
       params.require(:rental_listing).permit(
         :visible, :highlighted, :archived, :reserved, :furnished,
         :for_rent_short_term, :for_rent_long_term,
         :price_rental_monthly_current_cents, :price_rental_monthly_current_currency,
-        :price_rental_monthly_low_season_cents, :price_rental_monthly_high_season_cents
+        :price_rental_monthly_low_season_cents, :price_rental_monthly_high_season_cents,
+        *locale_fields
       )
+    end
+
+    def update_features(features_param)
+      # features_param is an array of selected feature keys
+      selected_features = Array(features_param)
+
+      # Remove features that are no longer selected
+      @prop.features.where.not(feature_key: selected_features).destroy_all
+
+      # Add new features
+      selected_features.each do |feature_key|
+        @prop.features.find_or_create_by(feature_key: feature_key)
+      end
     end
   end
 end
