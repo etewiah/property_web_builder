dd68ca91a966f84b6cc29205765fa27e20de7cc8
Ed Tewiah <etewiah@hotmail.com>
2025-12-16 13:09:22 +0000

    Adding subscription info to site admin dashboard controller

--- app/controllers/site_admin/dashboard_controller.rb
+++ app/controllers/site_admin/dashboard_controller.rb
@@ -24,6 +24,23 @@ module SiteAdmin
       @recent_properties = Pwb::ListedProperty.where(website_id: website_id).order(created_at: :desc).limit(5)
       @recent_messages = Pwb::Message.where(website_id: website_id).order(created_at: :desc).limit(5)
       @recent_contacts = Pwb::Contact.where(website_id: website_id).order(created_at: :desc).limit(5)
+
+      # Subscription information for current website
+      @subscription = Pwb::Subscription.find_by(website_id: website_id)
+      if @subscription
+        @subscription_info = {
+          status: @subscription.status,
+          plan_name: @subscription.plan.display_name,
+          plan_price: @subscription.plan.formatted_price,
+          trial_days_remaining: @subscription.trial_days_remaining,
+          trial_ending_soon: @subscription.trial_ending_soon?,
+          in_good_standing: @subscription.in_good_standing?,
+          current_period_ends_at: @subscription.current_period_ends_at,
+          property_limit: @subscription.plan.property_limit,
+          remaining_properties: @subscription.remaining_properties,
+          features: @subscription.plan.enabled_features
+        }
+      end
     end
   end
 end
