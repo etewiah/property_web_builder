bebbd291a9d84d02d435681e92f756a5f0a26c04
Ed Tewiah <etewiah@hotmail.com>
2025-12-17 09:15:45 +0000

    Using global ApplicationRecord

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Ahoy
-  class Visit < ApplicationRecord
+  class Visit < ::ApplicationRecord
     self.table_name = "ahoy_visits"
 
     # Multi-tenant associations
