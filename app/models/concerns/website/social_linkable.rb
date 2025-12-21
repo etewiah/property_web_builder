ba5bbc1794a89dd83210e4f8cf39e43dc05c5088
Ed Tewiah <etewiah@hotmail.com>
2025-12-21 22:55:55 +0100

    Create SocialLinkable concern for Website model

---
+++ app/models/concerns/website/social_linkable.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Website::SocialLinkable
+#
+# Provides social media link accessors for websites.
+# Retrieves social media URLs from the links association.
+#
+module Website
+  module SocialLinkable
+    extend ActiveSupport::Concern
+
+    def social_media_facebook
+      links.find_by(slug: "social_media_facebook")&.link_url
+    end
+
+    def social_media_twitter
+      links.find_by(slug: "social_media_twitter")&.link_url
+    end
+
+    def social_media_linkedin
+      links.find_by(slug: "social_media_linkedin")&.link_url
+    end
+
+    def social_media_youtube
+      links.find_by(slug: "social_media_youtube")&.link_url
+    end
+
+    def social_media_pinterest
+      links.find_by(slug: "social_media_pinterest")&.link_url
+    end
+  end
+end
