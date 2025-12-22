3a57f62faf0a34efbf71ce59969a6a3f50c1a536
Ed Tewiah <etewiah@hotmail.com>
2025-12-22 08:37:40 +0100

    Updating comment to remove Berlin reference

--- app/controllers/pwb/contact_us_controller.rb
+++ app/controllers/pwb/contact_us_controller.rb
@@ -135,7 +135,7 @@ module Pwb
     def header_image_url
       # lc_content = Content.where(tag: 'landing-carousel')[0]
       lc_photo = ContentPhoto.find_by_block_key "landing_img"
-      # used by berlin theme
+      # used for header background images
       @header_image_url = lc_photo.present? ? lc_photo.optimized_image_url : nil
     end
   end
