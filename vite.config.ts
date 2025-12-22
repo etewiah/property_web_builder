642ecea487642ddd5b25512fed2fd3ca83705ecb
Ed Tewiah <etewiah@hotmail.com>
2025-12-22 21:06:41 +0100

    Update vite.config.ts to remove Vue and Quasar plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
-import VuePlugin from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  define: {
-    // Vue 3 feature flags for production - suppresses dev mode warnings
-    // See: https://vuejs.org/api/compile-time-flags.html
-    __VUE_OPTIONS_API__: true,
-    __VUE_PROD_DEVTOOLS__: false,
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
-  },
   plugins: [
     RubyPlugin(),
-    VuePlugin({
-      template: { transformAssetUrls }
-    }),
-    quasar({
-      // sassVariables: 'src/quasar-variables.sass'
-    }),
     // Bundle analyzer - run with ANALYZE=true to generate report
     process.env.ANALYZE && visualizer({
       open: true,
