9ca97cec3f6b91d462334ef5cceefb2f060c3df7
Ed Tewiah <etewiah@hotmail.com>
2025-12-25 20:12:19 +0000

    Create agency controller

---
+++ app/controllers/site_admin/agency_controller.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module SiteAdmin
+  # AgencyController
+  # Manages the agency/company profile for the current website
+  class AgencyController < SiteAdminController
+    before_action :set_agency
+
+    def edit
+      # @agency set by before_action
+    end
+
+    def update
+      if @agency.update(agency_params)
+        redirect_to edit_site_admin_agency_path, notice: 'Agency profile updated successfully.'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def set_agency
+      @agency = current_website.agency || current_website.create_agency!
+    end
+
+    def agency_params
+      params.require(:pwb_agency).permit(
+        :display_name,
+        :company_name,
+        :email_primary,
+        :email_for_general_contact_form,
+        :email_for_property_contact_form,
+        :phone_number_primary,
+        :phone_number_mobile,
+        :phone_number_other,
+        :skype,
+        :url
+      )
+    end
+  end
+end
