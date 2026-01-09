5858767ab13b54ea3872964f8842be68fb54a679
Ed Tewiah <etewiah@hotmail.com>
2026-01-09 12:49:37 +0000

    Fix flowbite import

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,5 +10,5 @@ pin_all_from "app/javascript/services", under: "services"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.3/app/assets/javascripts/rails-ujs.esm.js"
 
 # Flowbite - UI components (Carousel, Dropdown)
-# Using esm.sh for ES module compatibility with importmap
-pin "flowbite", to: "https://esm.sh/flowbite@2.5.2/dist/flowbite.es.js"
+# Using jsDelivr ESM build for v4.0.1 compatibility
+pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@4.0.1/+esm"
