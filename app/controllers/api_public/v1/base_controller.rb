0bb377c6f7113b4af953f932317aa2b49f29fc25
Ed Tewiah <etewiah@hotmail.com>
2026-01-17 11:37:37 +0000

    Default caching for api_public endpoints

--- app/controllers/api_public/v1/base_controller.rb
+++ app/controllers/api_public/v1/base_controller.rb
@@ -2,7 +2,22 @@ module ApiPublic
   module V1
     class BaseController < ActionController::Base
       include SubdomainTenant
+
       skip_before_action :verify_authenticity_token
+
+      # Set HTTP cache headers and conditional GET for all api_public endpoints
+      before_action :set_api_public_cache_headers
+
+      private
+
+      def set_api_public_cache_headers
+        expires_in 5.hours, public: true
+        # Conditional GET: Use a generic last-modified time (can be overridden in child controllers)
+        return unless respond_to?(:resource_last_modified, true)
+
+        last_modified = resource_last_modified
+        fresh_when(last_modified: last_modified) if last_modified
+      end
     end
   end
 end
